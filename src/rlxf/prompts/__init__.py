939225c8118f56b939497cdb08111580edffb500
Alvaro Bartolome <alvaro@argilla.io>
2023-10-18 16:25:30 +0200

    Add `prompts` with `Llama2Prompt`

---
+++ src/rlxf/prompts/__init__.py
@@ -0,0 +1,4 @@
+from .llama2 import Llama2Prompt
+
+
+__all__ = ["Llama2Prompt"]
---
+++ src/rlxf/prompts/llama2.py
@@ -0,0 +1,13 @@
+class Llama2Prompt:
+    system_prompt: str = (
+        "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible,"
+        " while being safe. Your answers should not include any harmful, unethical, racist, sexist,"
+        " toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased"
+        " and positive in nature.\nIf a question does not make any sense, or is not factually coherent,"
+        " explain why instead of answering something not correct. If you don't know the answer to a"
+        " question, please don't share false information."
+    )
+
+    @staticmethod
+    def chat_format(instruction: str) -> str:
+        return "<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{instruction} [/INST]".format(system_prompt=Llama2Prompt.system_prompt, instruction=instruction)
