cdfc83752538f17b63c6be09b4b581543638b473
Alvaro Bartolome <alvaro@argilla.io>
2023-10-19 16:24:40 +0200

    Update `Prompt` return type-hints

--- src/rlxf/prompts/base.py
+++ src/rlxf/prompts/base.py
@@ -5,10 +5,10 @@ from typing import Any
 class Prompt(ABC):
     @staticmethod
     @abstractmethod
-    def chat_format(instruction: str, *args: Any, **kwargs: Any) -> str:
+    def chat_format(instruction: str, *args: Any, **kwargs: Any) -> Any:
         pass
 
     @staticmethod
     @abstractmethod
-    def rank_format(prompt: str, responses: list[str]) -> str:
+    def rank_format(prompt: str, responses: list[str]) -> Any:
         pass
