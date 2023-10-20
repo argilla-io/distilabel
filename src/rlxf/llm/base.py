fab0e9c4badf44f4e266c9a2e5b1b4083b71c377
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2023-10-20 17:28:13 +0200

    Add `LlamaCppLLM` with `PromptTemplate`

--- src/rlxf/llm/base.py
+++ src/rlxf/llm/base.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from rlxf.prompts.base import PromptTemplate
+
 
 class LLM(ABC):
     def __init__(self, prompt_template: PromptTemplate) -> None:
         self.prompt_template = prompt_template
 
     @abstractmethod
-    def generate(
-        self, prompts: list[str], responses: list[list[str]] | None = None
-    ) -> list[str]:
+    def generate(self, prompts: list[dict[str, Any]]) -> Any:
         pass
