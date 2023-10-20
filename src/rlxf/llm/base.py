0b06bbebd0bdda48c5a3717625a957ff6413bd0c
Alvaro Bartolome <alvaro@argilla.io>
2023-10-20 15:22:00 +0200

    Refactor `LLM` base class
    Still under active development
    
    Co-authored-by: Gabriel Martín <gabriel@argilla.io>

--- src/rlxf/llm/base.py
+++ src/rlxf/llm/base.py
@@ -1,27 +1,16 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Any, Generator
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from rlxf.prompts.base import PromptTemplate
 
 class LLM(ABC):
+    def __init__(self, prompt_template: PromptTemplate) -> None:
+        self.prompt_template = prompt_template
+
     @abstractmethod
-    def batch_generate(
+    def generate(
         self, prompts: list[str], responses: list[list[str]] | None = None
-    ) -> Generator[list[str], None, None]:
+    ) -> list[str]:
         pass
-
-    @classmethod
-    @abstractmethod
-    def as_generator(cls, *args: Any, **kwargs: Any) -> "LLM":
-        pass
-
-    @classmethod
-    @abstractmethod
-    def as_ranker(cls, *args: Any, **kwargs: Any) -> "LLM":
-        pass
-
-    def _batch_iterator(
-        self, prompts: list[str], responses: list[list[str]] | None = None
-    ) -> Generator[tuple[str, int | list[str]], None, None]:
-        yield from zip(
-            prompts, responses if responses is not None else range(len(prompts))
-        )
