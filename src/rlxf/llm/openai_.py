d651af0050b9837c153d60c26c9d5095483b398b
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2023-10-23 15:33:19 +0200

    Add handling OpenAI API exceptions

--- src/rlxf/llm/openai_.py
+++ src/rlxf/llm/openai_.py
@@ -4,11 +4,29 @@ import os
 from typing import TYPE_CHECKING, Any, Literal
 
 import openai
+from tenacity import (
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_random_exponential,
+)
 
 from rlxf.llm.base import LLM
 
 if TYPE_CHECKING:
     from rlxf.prompts.base import PromptTemplate
+
+from openai.error import APIError, RateLimitError, ServiceUnavailableError, Timeout
+
+_OPENAI_API_RETRY_ON_EXCEPTIONS = (
+    APIError,
+    Timeout,
+    RateLimitError,
+    ServiceUnavailableError,
+)
+_OPENAI_API_STOP_AFTER_ATTEMPT = 6
+_OPENAI_API_WAIT_RANDOM_EXPONENTIAL_MULTIPLIER = 1
+_OPENAI_API_WAIT_RANDOM_EXPONENTIAL_MAX = 10
 
 
 class OpenAILLM(LLM):
@@ -35,16 +53,25 @@ class OpenAILLM(LLM):
             openai.api_key is not None
         ), "Either the `openai_api_key` arg or the `OPENAI_API_KEY` environment variable must be set to use the OpenAI API."
 
+    @retry(
+        retry=retry_if_exception_type(_OPENAI_API_RETRY_ON_EXCEPTIONS),
+        stop=stop_after_attempt(_OPENAI_API_STOP_AFTER_ATTEMPT),
+        wait=wait_random_exponential(
+            multiplier=_OPENAI_API_WAIT_RANDOM_EXPONENTIAL_MULTIPLIER,
+            max=_OPENAI_API_WAIT_RANDOM_EXPONENTIAL_MAX,
+        ),
+    )
+    def _chat_completion_with_backoff(self, **kwargs: Any) -> Any:
+        return openai.ChatCompletion.create(**kwargs)
+
     def generate(self, inputs: list[dict[str, Any]]) -> Any:
         generations = []
         for prompt in inputs:
             prompt = self.prompt_template.generate_prompt(**prompt)
-            output = openai.ChatCompletion.create(
-                model=self.model,
-                messages=prompt,
-            )[
-                "choices"
-            ][0]["message"]["content"].strip()
+            response = self._chat_completion_with_backoff(
+                model=self.model, messages=prompt
+            )
+            output = response["choices"][0]["message"]["content"].strip()
             output = self.prompt_template.parse_output(output)
             generations.append(output)
         return generations
