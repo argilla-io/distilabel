48b7b49fc1fbda04898a191c8329b687f2de73f8
Alvaro Bartolome <alvaro@argilla.io>
2023-10-24 11:58:50 +0200

    Add `concurrent.futures` and `tenacity` to `InferenceEndpointsLLM`

--- src/rlxf/llm/huggingface.py
+++ src/rlxf/llm/huggingface.py
@@ -1,14 +1,36 @@
+from concurrent.futures import ThreadPoolExecutor
 from functools import cached_property
 from typing import TYPE_CHECKING, Any, Dict, List, Union
 
 import torch
-from huggingface_hub import InferenceClient
+from huggingface_hub import (
+    InferenceClient,
+    InferenceTimeoutError,
+    RateLimitError,
+    ServiceUnavailableError,
+)
+from tenacity import (
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_random_exponential,
+)
 from transformers import PreTrainedModel, PreTrainedTokenizer
 
 from rlxf.llm.base import LLM
 
 if TYPE_CHECKING:
     from rlxf.prompts.base import PromptTemplate
+
+
+_INFERENCE_ENDPOINTS_API_RETRY_ON_EXCEPTIONS = (
+    InferenceTimeoutError,
+    RateLimitError,
+    ServiceUnavailableError,
+)
+_INFERENCE_ENDPOINTS_API_STOP_AFTER_ATTEMPT = 6
+_INFERENCE_ENDPOINTS_API_WAIT_RANDOM_EXPONENTIAL_MULTIPLIER = 1
+_INFERENCE_ENDPOINTS_API_WAIT_RANDOM_EXPONENTIAL_MAX = 10
 
 
 class TransformersLLM(LLM):
@@ -69,16 +91,62 @@ class InferenceEndpointsLLM(LLM):
         endpoint_url: str,
         prompt_template: "PromptTemplate",
         token: Union[str, None] = None,
+        max_new_tokens: int = 128,
+        temperature: float = 1.0,
+        num_threads: Union[int, None] = None,
     ) -> None:
         super().__init__(prompt_template=prompt_template)
 
         self.client = InferenceClient(model=endpoint_url, token=token)
+        self.max_new_tokens = max_new_tokens
+        self.temperature = temperature
 
-    def generate(self, inputs: List[Dict[str, Any]]) -> Any:
-        # TODO(alvarobartt): add `generation_kwargs`
+        # TODO(alvarobartt,gabrielmbmb): move to `LLM` base class defintion
+        self.thread_pool_executor = (
+            ThreadPoolExecutor(max_workers=num_threads)
+            if num_threads is not None
+            else None
+        )
+
+    def __del__(self) -> None:
+        if self.thread_pool_executor is not None:
+            self.thread_pool_executor.shutdown()
+
+    @retry(
+        retry=retry_if_exception_type(_INFERENCE_ENDPOINTS_API_RETRY_ON_EXCEPTIONS),
+        stop=stop_after_attempt(_INFERENCE_ENDPOINTS_API_STOP_AFTER_ATTEMPT),
+        wait=wait_random_exponential(
+            multiplier=_INFERENCE_ENDPOINTS_API_WAIT_RANDOM_EXPONENTIAL_MULTIPLIER,
+            max=_INFERENCE_ENDPOINTS_API_WAIT_RANDOM_EXPONENTIAL_MAX,
+        ),
+    )
+    def _text_generation_with_backoff(self, **kwargs: Any) -> Any:
+        return self.client.text_generation(
+            **kwargs, max_new_tokens=self.max_new_tokens, temperature=self.temperature
+        )
+
+    def _generate(
+        self, input: Dict[str, Any], num_generations: int = 1
+    ) -> List[Dict[str, Any]]:
+        prompt = self.prompt_template.generate_prompt(**input)
+        responses = [
+            self._text_generation_with_backoff(prompt=prompt)
+            for _ in range(num_generations)
+        ]
+        return [self.prompt_template.parse_output(response) for response in responses]
+
+    def generate(self, inputs: List[Dict[str, Any]], num_generations: int = 1) -> Any:
+        if self.thread_pool_executor is not None:
+            return [
+                self.thread_pool_executor.submit(self._generate, input, num_generations)
+                for input in inputs
+            ]
         generations = []
         for input in inputs:
-            prompt = self.prompt_template.generate_prompt(**input)
-            generation = self.client.text_generation(prompt)
-            generations.append(self.prompt_template.parse_output(generation))
+            output = self._generate(input, num_generations)
+            generations.append(output)
         return generations
+
+    @property
+    def return_futures(self) -> bool:
+        return self.thread_pool_executor is not None
