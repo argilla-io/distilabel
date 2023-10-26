45b7604afa1f0a61e227af3961da60e3ebac4aab
Alvaro Bartolome <alvaro@argilla.io>
2023-10-26 12:01:20 +0200

    Add `to_argilla_{fields,questions,record}` methods in `OpenAIResponseRanking` (WIP)

--- src/rlxf/prompts/openai_.py
+++ src/rlxf/prompts/openai_.py
@@ -1,11 +1,24 @@
 import importlib.resources as importlib_resources
 from textwrap import dedent
-from typing import List, Literal, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Literal, Type, Union
 
 import jinja2
 from typing_extensions import TypedDict
 
 from rlxf.prompts.base import PromptTemplate
+
+try:
+    import argilla as rg
+
+    _argilla_installed = True
+except ImportError:
+    _argilla_installed = False
+
+if TYPE_CHECKING:
+    from argilla.client.feedback.schemas.types import (
+        AllowedFieldTypes,
+        AllowedQuestionTypes,
+    )
 
 _GPT4_RANKING_TEMPLATE = str(
     importlib_resources.files("rlxf") / "prompts/templates/gpt4-response-ranking.jinja2"
@@ -88,6 +101,63 @@ class OpenAIResponseRanking(PromptTemplate):
     @property
     def output_args_names(self) -> List[str]:
         return ["rating", "rationale"]
+
+    @property
+    def argilla_fields_typedargs(self) -> Dict[str, Union[Type[str], Type[list]]]:
+        # If a `List[str]` is provided, then it means that the field will be generated
+        # appending an integer from 1 to N e.g. `generations` -> `generations-1`, `generations-2`, etc.
+        return {"instruction": str, "generations": list}
+
+    def to_argilla_fields(self, dataset_row: Dict[str, Any]) -> List["AllowedFieldTypes"]:
+        argilla_fields = []
+        for arg_name, arg_type in self.argilla_fields_typedargs.items():
+            if arg_name not in dataset_row:
+                raise ValueError(
+                    f"Dataset row does not contain the required field '{arg_name}'."
+                )
+            if arg_type is list and isinstance(dataset_row[arg_name], list):
+                for idx in range(1, len(dataset_row[arg_name]) + 1):
+                    argilla_fields.append(rg.TextField(name=f"{arg_name}-{idx}"))
+            elif arg_type is str:
+                argilla_fields.append(rg.TextField(name=arg_name))
+            else:
+                raise ValueError(f"Type {arg_type} is not supported.")
+        return argilla_fields
+
+    def to_argilla_questions(self, dataset_row: Dict[str, Any]) -> List["AllowedQuestionTypes"]:
+        if not _argilla_installed:
+            raise ImportError("The argilla library is not installed.")
+        argilla_questions = []
+        for idx in range(1, len(dataset_row["generations"]) + 1):
+            argilla_questions.extend(
+                [
+                    rg.RatingQuestion(
+                        name=f"generations-{idx}-rating",
+                        title=f"Whats's the rating for generations-{idx}?",
+                        values=list(range(1, len(self.ranks) + 1)),
+                    ),
+                    rg.TextQuestion(
+                        name=f"generations-{idx}-rationale",
+                        title=f"Whats's the rationale behind generations-{idx}'s rating?",
+                    ),
+                ]
+            )
+        return argilla_questions
+
+    def to_argilla_record(self, dataset_row: Dict[str, Any]) -> rg.FeedbackRecord:
+        fields = {}
+        for input_arg_key, input_arg_value in self.argilla_fields_typedargs.items():
+            if input_arg_value is list:
+                for idx in range(1, len(dataset_row[input_arg_key]) + 1):
+                    fields.update({f"{input_arg_key}-{idx}": dataset_row[input_arg_key][idx - 1]})
+            else:
+                fields.update({input_arg_key: dataset_row[input_arg_key]})
+        response = {"values": {}, "status": "submitted"}
+        for output_arg_name in self.output_args_names:
+            for idx, value in enumerate(dataset_row[output_arg_name]):
+                response["values"].update({f"generations-{idx}-{output_arg_name}": {"value": value}})
+        return rg.FeedbackRecord(fields=fields, responses=[response])
+
 
 
 class OpenAITextGenerationPromptTemplate(PromptTemplate):
