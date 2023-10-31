f941627b2a1b4e99266ff49b944fad2c1b1bdea1
Alvaro Bartolome <alvaro@argilla.io>
2023-10-31 14:52:07 +0100

    Add `Prompt` with `{system,formatted}_prompt` and `format_as` method

--- src/ultralabel/tasks/utils.py
+++ src/ultralabel/tasks/utils.py
@@ -1,8 +1,32 @@
-from typing import Literal
+from typing import List, Literal, Union
 
+from pydantic import BaseModel
 from typing_extensions import TypedDict
 
 
 class ChatCompletion(TypedDict):
     role: Literal["system", "user", "assistant"]
     content: str
+
+
+class Prompt(BaseModel):
+    system_prompt: str
+    formatted_prompt: str
+
+    def format_as(
+        self, format: Literal["openai", "llama2"]
+    ) -> Union[str, List[ChatCompletion]]:
+        if format == "openai":
+            return [
+                ChatCompletion(
+                    role="system",
+                    content=self.system_prompt,
+                ),
+                ChatCompletion(role="user", content=self.formatted_prompt),
+            ]
+        elif format == "llama2":
+            return f"<s>[INST] <<SYS>>\n{self.system_prompt}<</SYS>>\n\n{self.formatted_prompt} [/INST]"
+        else:
+            raise ValueError(
+                f"Format {format} not supported, please provide a custom `formatting_fn`."
+            )
