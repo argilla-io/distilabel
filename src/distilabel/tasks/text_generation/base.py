cdc9c9eb5f3023c11b880cf9ab4cadb615c02322
Daniel Vila Suero <daniel@argilla.io>
2023-11-15 10:31:52 +0100

    Adds `principles` to `TextGenerationTask` (#87)
    * Add principles for text generation
    
    * Move `principles` logic to base `TextGenerationTask`
    
    * Update if condition
    
    * Change name to `UltraFeedbackPrinciples`
    
    ---------
    
    Co-authored-by: gabrielmbmb <gmartinbdev@gmail.com>

--- src/distilabel/tasks/text_generation/base.py
+++ src/distilabel/tasks/text_generation/base.py
@@ -12,10 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import random
+from dataclasses import dataclass, field
+from typing import Dict, List, Literal, Union
+
 from distilabel.tasks.base import Task
 from distilabel.tasks.prompt import Prompt
+from distilabel.tasks.text_generation.principles import UltraFeedbackPrinciples
 
 
+@dataclass
 class TextGenerationTask(Task):
     system_prompt: str = (
         "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible,"
@@ -25,9 +31,57 @@ class TextGenerationTask(Task):
         " explain why instead of answering something not correct. If you don't know the answer to a"
         " question, please don't share false information."
     )
+    principles: Dict[str, List[str]] = field(
+        default_factory=lambda: {
+            "harmlessness": UltraFeedbackPrinciples.harmlessness,
+            "helpfulness": UltraFeedbackPrinciples.helpfulness,
+            "truthfulness": UltraFeedbackPrinciples.truthfulness,
+            "honesty": UltraFeedbackPrinciples.honesty,
+            "verbalized_calibration": UltraFeedbackPrinciples.verbalized_calibration,
+        },
+        repr=False,
+    )
+    principles_distribution: Union[Dict[str, float], Literal["balanced"], None] = None
+
+    def __post_init__(self) -> None:
+        if isinstance(self.principles_distribution, dict):
+            not_included_principles = [
+                principle
+                for principle in self.principles
+                if principle not in self.principles_distribution
+            ]
+            if not_included_principles:
+                principles_str = ", ".join(
+                    [f"'{principle}'" for principle in not_included_principles]
+                )
+                raise ValueError(
+                    f"Principles {principles_str} included in `principles` is not in"
+                    " `principles_distribution`"
+                )
+
+            if sum(self.principles_distribution.values()) != 1.0:
+                raise ValueError(
+                    "`principles_distribution` must sum to 1.0 if it is a dict containing"
+                    " the distribution of principles to use."
+                )
+
+    def _get_principle(self) -> str:
+        if isinstance(self.principles_distribution, dict):
+            principle_group = random.choices(
+                list(self.principles_distribution.keys()),
+                weights=list(self.principles_distribution.values()),
+                k=1,
+            )[0]
+        else:
+            principle_group = random.choice(list(self.principles.keys()))
+        return random.choice(self.principles[principle_group])
 
     def generate_prompt(self, input: str) -> Prompt:
-        return Prompt(system_prompt=self.system_prompt, formatted_prompt=input)
+        system_prompt = self.system_prompt
+        if self.principles_distribution is not None:
+            principle = self._get_principle()
+            system_prompt += " " + principle
+        return Prompt(system_prompt=system_prompt, formatted_prompt=input)
 
     def parse_output(self, output: str) -> dict[str, str]:
         return {"generations": output}
