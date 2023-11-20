47739818aaf98d7c139d6abe8b9db6ad8ed3a479
Alvaro Bartolome <alvaro@argilla.io>
2023-11-20 11:45:15 +0100

    Rename `vllm->vllm_` and add disclaimer on naming

--- src/distilabel/llm/vllm_.py
+++ src/distilabel/llm/vllm_.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# WARNING: THIS FILE NAME HAS BEEN PREPENDED WITH AN UNDERSCORE TO AVOID
+# ANY POTENTIAL CONFLICT / COLLISSION WITH THE `vllm` PYTHON PACKAGE.
 
 from typing import TYPE_CHECKING, Any, Callable, Dict, List, Union
 
