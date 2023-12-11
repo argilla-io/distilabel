1f041eeaa454d663e29b3c03182a7610bf10d2f5
Edward Beeching <edbeeching@users.noreply.github.com>
2023-12-11 11:14:31 +0100

    Update examples/pipeline-accelerate-and-openai.py
    Co-authored-by: Alvaro Bartolome <alvarobartt@gmail.com>

--- examples/pipeline-accelerate-and-openai.py
+++ examples/pipeline-accelerate-and-openai.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
             task=UltraFeedbackTask.for_instruction_following(),
             max_new_tokens=128,
             num_threads=2,
-            openai_api_key="<OPENAI_API_KEY>",
+            openai_api_key=os.getenv("OPENAI_API_KEY", None),
             temperature=0.0,
         ),
     )
