5d6a9df21a232151e44821368ce8333bbc611812
Alvaro Bartolome <alvaro@argilla.io>
2023-12-13 16:25:23 +0100

    Add `UltraCMTask` (WIP)

---
+++ src/distilabel/tasks/critique/ultracm.py
@@ -0,0 +1,55 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import ClassVar
+
+from typing_extensions import TypedDict
+
+from distilabel.tasks.base import get_template
+from distilabel.tasks.critique.base import CritiqueTask
+
+_ULTRACM_TEMPLATE = get_template("ultracm.jinja2")
+
+
+class UltraCMOutput(TypedDict):
+    """A `TypedDict` matching the output format of UltraCM."""
+
+    score: float
+    critique: str
+
+
+@dataclass
+class UltraCMTask(CritiqueTask):
+    __jinja2_template__: ClassVar[str] = _ULTRACM_TEMPLATE
+
+    system_prompt: str = (
+        "User: A one-turn chat between a curious user and an artificial intelligence"
+        " assistant. The assistant gives helpful, very detailed, and polite answers to"
+        " the user's questions.</s>"
+    )
+
+    def generate_prompt(self, instruction: str, completion: str) -> str:
+        render_kwargs = {
+            "instruction": instruction,
+            "completion": completion,
+        }
+        return f"{self.system_prompt}\nUser: {self.template.render(**render_kwargs)}</s>\nAssistant: "
+
+    def parse_output(self, output: str) -> UltraCMOutput:
+        """Parses the output of the model into the desired format."""
+        split_output = output.split("\n")
+        score = float(split_output[0].strip())
+        critique = "\n".join(split_output[1:])
+        return UltraCMOutput(score=score, critique=critique)
