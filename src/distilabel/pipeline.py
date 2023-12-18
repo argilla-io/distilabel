argilla-io/distilabel

commit 05b7285b07a6020a00d3e034669f743da629c179

Add `ProcessLLM` class (#151)
* Add `ProcessLLM` class

* Update `logging` config to also log PID

* Finalize integration of `ProcessLLM` with `Pipeline`

* Remove `verbose` parameter

* Change `ProcessLLM` log messages level to `debug`

* Add docstrings for `ProcessLLM`

* Add silencing `httpx` log messages

* Add `ProcessLLM` example

* Add missing docstrings

* Apply suggestions from code review

Co-authored-by: Alvaro Bartolome <alvaro@argilla.io>

* Fix `labeller` type hints in `Pipeline`

* Add hook to add license header automatically

---------

Co-authored-by: Alvaro Bartolome <alvaro@argilla.io>

src/distilabel/pipeline.py
@@ -17,7 +17,6 @@ import os
 import warnings
 from concurrent.futures import Future
 from typing import (
-    TYPE_CHECKING,
     Any,
     Callable,
     Dict,
@@ -26,11 +25,13 @@ from typing import (
     Literal,
     Optional,
     Union,
+    cast,
 )
 
 from datasets import Dataset, Split
 
 from distilabel.dataset import CustomDataset
+from distilabel.llm.base import LLM, LLMFutures, ProcessLLM
 from distilabel.llm.utils import LLMOutput
 from distilabel.logger import get_logger
 from distilabel.progress_bar import (
@@ -39,10 +40,7 @@ from distilabel.progress_bar import (
     use_progress_bar,
 )
 from distilabel.utils.dicts import combine_dicts
-
-if TYPE_CHECKING:
-    from distilabel.llm.base import LLM
-
+from distilabel.utils.types import is_list_of_futures
 
 logger = get_logger()
 
@@ -50,8 +48,8 @@ logger = get_logger()
 class Pipeline:
     def __init__(
         self,
-        generator: Union["LLM", None] = None,
-        labeller: Union["LLM", None] = None,
+        generator: Union["LLM", "ProcessLLM", None] = None,
+        labeller: Union["LLM", "ProcessLLM", None] = None,
     ) -> None:
         """Initializes the Pipeline class.
 
@@ -87,7 +85,7 @@ class Pipeline:
         self.labeller = labeller
 
         if self.generator is None and self.labeller is None:
-            raise ValueError("At least one LLM has to be provided to the pipeline")
+            raise ValueError("Either `generator` or `labeller` must be provided.")
 
     def __repr__(self) -> str:
         return (
@@ -169,6 +167,7 @@ class Pipeline:
         self,
         inputs: List[Dict[str, Any]],
         num_generations: int,
+        num_batches: int,
         progress_callback_func: Union[Callable, None] = None,
     ) -> List[Dict[str, Any]]:
         """Gets the batch generations for the given inputs, capturing the futures if the
@@ -185,21 +184,90 @@ class Pipeline:
         Returns:
             List[Dict[str, Any]]: the processed batch generations.
         """
-        batch_generations = self.generator.generate(
+        outputs = self.generator.generate(
             inputs=inputs,
             num_generations=num_generations,
             progress_callback_func=progress_callback_func,
         )
-
-        processed_generations = []
-        if self.generator.return_futures:  # type: ignore
-            for future in batch_generations:
-                result = future.result()
-                processed_generations.extend(result)
+        batch_generations = []
+        if isinstance(outputs, Future):
+            try:
+                # Result of future is `List[List[LLMOutput]]` (first list contains `batch_size`
+                # elements, and the second list contains `num_generations` elements)
+                batch_generations.extend(outputs.result())
+            except Exception as e:
+                logger.error(
+                    f"An error occured when getting the result from the generator: {e}"
+                )
+                batch_generations.extend(
+                    [
+                        [
+                            LLMOutput(
+                                model_name=self.generator.model_name,
+                                prompt_used=None,
+                                raw_output=None,
+                                parsed_output=None,
+                            )
+                            for _ in range(num_generations)
+                        ]
+                        for _ in range(num_batches)
+                    ]
+                )
+        elif is_list_of_futures(outputs):
+            for future in outputs:
+                # Result of future is `List[List[LLMOutput]]` (first list contains 1
+                # element, and the second list contains `num_generations` elements)
+                try:
+                    batch_generations.extend(future.result())
+                except Exception as e:
+                    logger.error(
+                        f"An error ocurred when getting the result of a future from the generator: {e}"
+                    )
+                    batch_generations.extend(
+                        [
+                            LLMOutput(
+                                model_name=self.generator.model_name,
+                                prompt_used=None,
+                                raw_output=None,
+                                parsed_output=None,
+                            )
+                            for _ in range(num_generations)
+                        ]
+                    )
         else:
-            processed_generations = batch_generations
+            batch_generations = outputs
+        return self._process_batch_generations(batch_generations=batch_generations)
 
-        return self._process_batch_generations(batch_generations=processed_generations)
+    def _get_batch_labels(
+        self,
+        inputs: List[Dict[str, Any]],
+        progress_callback_func: Union[Callable, None] = None,
+    ) -> Union[
+        Future[List[List["LLMOutput"]]],
+        List[Future[List[List["LLMOutput"]]]],
+        List[List["LLMOutput"]],
+    ]:
+        """Gets the batch labels for the given inputs.
+
+        Args:
+            inputs (List[Dict[str, Any]]): the inputs to be used for labelling. Each dict
+                should contain a key with the text generations.
+            progress_callback_func (Union[Callable, None], optional): the callback function
+                to be called when the progress of the labelling process changes. Defaults
+                to `None`.
+
+        Returns:
+            Union[Future[List["LLMOutput"]], List[Future], List["LLMOutput"]]: the batch
+                labels.
+        """
+
+        return self.labeller.generate(  # type: ignore
+            inputs=inputs,
+            # `num_generations` is always 1 because labelling the same input multiple times
+            # using the same LLM may not make sense
+            num_generations=1,
+            progress_callback_func=progress_callback_func,
+        )
 
     def _process_batch_generations(
         self,
@@ -225,6 +293,7 @@ class Pipeline:
                     generation["raw_output"] for generation in generations
                 ],
             }
+            # Create `generations` column which is a list with N text generations
             try:
                 processed_generation.update(
                     **combine_dicts(
@@ -342,7 +411,12 @@ class Pipeline:
         self,
         dataset: Dataset,
         generations: List[Dict[str, Any]],
-        batch_labels: Union[List[Future["LLMOutput"]], List["LLMOutput"]],
+        labels: Union[
+            Future[List[List["LLMOutput"]]],
+            List[Future[List[List["LLMOutput"]]]],
+            List[List["LLMOutput"]],
+        ],
+        batch_size: int,
     ) -> CustomDataset:
         """Builds the final dataset with either the generations, the labels, or both, depending
         on the LLMs provided to the `Pipeline`.
@@ -350,8 +424,8 @@ class Pipeline:
         Args:
             dataset (Dataset): the original dataset.
             generations (List[Dict[str, Any]]): the processed generations.
-            batch_labels (Union[List[Future["LLMOutput"]], List["LLMOutput"]]): the processed
-                batch labels.
+            labels (Union[List[Future["LLMOutput"]], List["LLMOutput"]]): the processed
+                labels.
 
         Returns:
             CustomDataset: the final dataset.
@@ -376,25 +450,38 @@ class Pipeline:
                     ]
                 )
 
+            # Add missing keys/columns with a `None` value
             for generation in generations:
                 for key in generator_column_names:
                     if key not in generation:
                         generation.update({key: None})
 
         if self.labeller is None:
-            labels = [{} for _ in range(len(dataset))]
+            processed_labels = [{} for _ in range(len(dataset))]  # type: ignore
         else:
-            # If the LLM returns futures, we need to wait for them to finish
-            processed_labels = []
-            if self.labeller.return_futures:
-                for future in batch_labels:
+            batch_labels = []
+            if self.labeller.return_futures is not None:
+                for i, future in enumerate(labels, start=1):  # type: ignore
                     try:
-                        processed_labels.extend(future.result())
+                        batch_labels.extend(future.result())
                     except Exception as e:
                         logger.error(
                             f"An error occurred when getting the result from the labeller: {e}"
                         )
-                        processed_labels.append(
+                        # If the LLM returned a list of futures (`LLM` with thread pool
+                        # executor), and each future contains just the result for a single
+                        # row, then we need to create an empty LLMOutput for each future.
+                        # If the LLM returns a future containing `batch_size` rows in the
+                        # result (`ProcessLLM`), then we need to create a list of empty
+                        # LLMOutputs with the length of the `batch_size`
+                        num_outputs = (
+                            1
+                            if self.labeller.return_futures == LLMFutures.CONTAINS_ROWS
+                            else batch_size
+                            if i * batch_size <= len(dataset)
+                            else len(dataset) % batch_size
+                        )
+                        batch_labels.append(
                             [
                                 LLMOutput(
                                     model_name=self.labeller.model_name,
@@ -402,11 +489,13 @@ class Pipeline:
                                     raw_output=None,
                                     parsed_output=None,
                                 )
+                                for _ in range(num_outputs)
                             ]
                         )
-            else:
-                processed_labels = batch_labels
-            labels = self._process_batch_labels(batch_labels=processed_labels)  # type: ignore
+
+            processed_labels = self._process_batch_labels(
+                batch_labels=batch_labels or cast(List[List["LLMOutput"]], labels)
+            )
 
             labeller_column_names = [
                 "labelling_model",
@@ -416,16 +505,16 @@ class Pipeline:
 
             # Ensure the lengths of the labels and the dataset match (when pipeline
             # fails in an intermediate step, the labels may be shorter than the dataset)
-            if len(labels) < len(dataset):
-                labels.extend(
+            if len(processed_labels) < len(dataset):
+                processed_labels.extend(
                     [
                         {key: None for key in labeller_column_names}
-                        for _ in range(len(dataset) - len(labels))
+                        for _ in range(len(dataset) - len(processed_labels))
                     ]
                 )
 
             # Add missing keys/columns with a `None` value
-            for label in labels:
+            for label in processed_labels:
                 for key in labeller_column_names:
                     if key not in label:
                         label.update({key: None})
@@ -439,6 +528,13 @@ class Pipeline:
         _dataset.task = self.labeller.task if self.labeller is not None else None  # type: ignore
         return _dataset  # type: ignore
 
+    def _teardown(self) -> None:
+        if self.generator is not None and isinstance(self.generator, ProcessLLM):
+            self.generator.teardown()
+
+        if self.labeller is not None and isinstance(self.labeller, ProcessLLM):
+            self.labeller.teardown()
+
     def _generate(  # noqa: C901
         self,
         dataset: Dataset,
@@ -446,7 +542,6 @@ class Pipeline:
         batch_size: int = 1,
         enable_checkpoints: bool = True,
         display_progress_bar: bool = False,
-        verbose: bool = True,
     ) -> CustomDataset:
         """Generates the outputs for the given dataset using the LLMs provided to the `Pipeline`.
 
@@ -457,7 +552,6 @@ class Pipeline:
             batch_size (int, optional): the batch size to be used for generation. Defaults to `1`.
             enable_checkpoints (bool, optional): whether to enable checkpoints or not. Defaults to `True`.
             display_progress_bar (bool, optional): whether to display the progress bar or not. Defaults to `False`.
-            verbose (bool, optional): whether to display the logs or not. Defaults to `True`.
 
         Returns:
             CustomDataset: the final dataset.
@@ -486,9 +580,6 @@ class Pipeline:
             >>> pipeline = Pipeline(generator=generator, labeller=labeller)
             >>> dataset = pipeline.generate(dataset=..., num_generations=1, batch_size=1)
         """
-        if not verbose:
-            logger.setLevel("ERROR")
-
         if (
             self.labeller is not None
             and self.generator is not None
@@ -507,7 +598,11 @@ class Pipeline:
         self._validate_dataset(dataset)
 
         generations: List[Dict[str, Any]] = []
-        batch_labels: Union[Future[List["LLMOutput"]], List["LLMOutput"]] = []
+        labels: Union[
+            Future[List[List["LLMOutput"]]],
+            List[Future[List[List["LLMOutput"]]]],
+            List[List["LLMOutput"]],
+        ] = []
 
         (
             generation_progress_func,
@@ -528,7 +623,7 @@ class Pipeline:
                 logger.info(f"Calling generator for batch {batch_i}...")
                 try:
                     batch_generations = self._get_batch_generations(
-                        inputs, num_generations, generation_progress_func
+                        inputs, num_generations, num_batches, generation_progress_func
                     )
                     generations.extend(batch_generations)
                 except Exception as e:
@@ -540,7 +635,10 @@ class Pipeline:
                         f"`Pipeline.generate` failed during generation step with exception: {e}"
                     )
                     return self._build_dataset(
-                        dataset, generations=generations, batch_labels=batch_labels
+                        dataset,
+                        generations=generations,
+                        labels=labels,
+                        batch_size=batch_size,
                     )
 
                 inputs = self._include_generator_outputs_as_inputs(
@@ -550,16 +648,14 @@ class Pipeline:
             if self.labeller is not None:
                 logger.info(f"Calling labeller for batch {batch_i}...")
                 try:
-                    # TODO: move to `self._get_batch_labels` (without awaiting futures)
-                    batch_labels.extend(
-                        self.labeller.generate(  # type: ignore
-                            inputs=inputs,
-                            # `num_generations` is always 1 because labelling the same input multiple times
-                            # using the same LLM may not make sense
-                            num_generations=1,
-                            progress_callback_func=labelling_progress_func,
-                        )
+                    batch_labels = self._get_batch_labels(
+                        inputs=inputs, progress_callback_func=labelling_progress_func
                     )
+
+                    if isinstance(batch_labels, Future):
+                        labels.append(batch_labels)
+                    else:
+                        labels.extend(batch_labels)  # type: ignore
                 except Exception as e:
                     if not enable_checkpoints:
                         raise RuntimeError(
@@ -569,13 +665,16 @@ class Pipeline:
                         f"`Pipeline.generate` failed during labelling step with exception: {e}"
                     )
                     return self._build_dataset(
-                        dataset, generations=generations, batch_labels=batch_labels
+                        dataset,
+                        generations=generations,
+                        labels=labels,
+                        batch_size=batch_size,
                     )
 
         _pipeline_progress.stop()
 
         return self._build_dataset(
-            dataset, generations=generations, batch_labels=batch_labels
+            dataset, generations=generations, labels=labels, batch_size=batch_size
         )
 
     def generate(
@@ -626,6 +725,7 @@ class Pipeline:
             >>> dataset = pipeline.generate(dataset=..., num_generations=1, batch_size=1)
         """
         try:
+            logger.info("Executing dry-run...")
             # First we generate a `Dataset` only with the first row from the whole dataset
             subset = Dataset.from_dict(
                 {key: [value] for key, value in dataset[0].items()}
@@ -638,21 +738,28 @@ class Pipeline:
                 batch_size=1,
                 enable_checkpoints=False,
                 display_progress_bar=False,
-                verbose=False,
             )
         except Exception as e:
+            self._teardown()
             raise RuntimeError(
                 f"`Pipeline.generate` failed during the dry run over {dataset[0]} with exception: {e}"
             ) from e
 
-        return use_progress_bar(self._generate)(
+        logger.info(
+            "Dry-run executed with no issues. Starting the actual generation..."
+        )
+
+        dataset = use_progress_bar(self._generate)(
             dataset=dataset,
             num_generations=num_generations,
             batch_size=batch_size,
             enable_checkpoints=enable_checkpoints,
             display_progress_bar=display_progress_bar,
-            verbose=verbose,
         )
+
+        self._teardown()
+
+        return dataset
 
 
 def pipeline(

commit 0318b645fa04f063d94931a3770db9279194211d

Add `to_argilla_{dataset,record}` methods in `TextGenerationTask` (#159)
* Add `_dataset.task` when only `generator` is provided

* Add `to_argilla_{dataset,record}` methods in `TextGenerationTask`

* Fix `_build_dataset` since `processed_labels` was ignored

Bug introduced in https://github.com/argilla-io/distilabel/pull/151

* Fix `SelfInstructTask` Jinja2 template and spacing

* Remove `verbose` arg in `Pipeline._generate`

* Fix `to_argilla_record` for `SelfInstructTask`

src/distilabel/pipeline.py
@@ -527,7 +527,10 @@ class Pipeline:
         )  # type: ignore
         # Dynamically remaps the `datasets.Dataset` to be a `CustomDataset` instance
         _dataset.__class__ = CustomDataset
-        _dataset.task = self.labeller.task if self.labeller is not None else None  # type: ignore
+        if self.generator is not None and self.labeller is None:
+            _dataset.task = self.generator.task  # type: ignore
+        elif self.labeller is not None:
+            _dataset.task = self.labeller.task  # type: ignore
         return _dataset  # type: ignore
 
     def _teardown(self) -> None:
@@ -686,7 +689,6 @@ class Pipeline:
         batch_size: int = 1,
         enable_checkpoints: bool = True,
         display_progress_bar: bool = False,
-        verbose: bool = True,
     ) -> CustomDataset:
         """Generates the outputs for the given dataset using the LLMs provided to the `Pipeline`.
 
@@ -697,7 +699,6 @@ class Pipeline:
             batch_size (int, optional): the batch size to be used for generation. Defaults to `1`.
             enable_checkpoints (bool, optional): whether to enable checkpoints or not. Defaults to `True`.
             display_progress_bar (bool, optional): whether to display the progress bar or not. Defaults to `False`.
-            verbose (bool, optional): whether to display the logs or not. Defaults to `True`.
 
         Returns:
             CustomDataset: the final dataset.

src/distilabel/tasks/_templates/self-instruct.jinja2
@@ -1,5 +1,5 @@
 # Task Description
-Develop {{ num_examples }} user queries that can be received by the given AI application and applicable to the provided context. Emphasize diversity in verbs and linguistic structures within the model's textual capabilities.
+Develop {{ num_instructions }} user queries that can be received by the given AI application and applicable to the provided context. Emphasize diversity in verbs and linguistic structures within the model's textual capabilities.
 
 # Criteria for Queries
 Incorporate a diverse range of verbs, avoiding repetition.

src/distilabel/tasks/text_generation/base.py
@@ -13,12 +13,22 @@
 # limitations under the License.
 
 import random
+import warnings
 from dataclasses import dataclass, field
-from typing import Dict, List, Literal, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Literal, Optional, Union
 
+from distilabel.tasks.argilla_utils import infer_fields_from_dataset_row
 from distilabel.tasks.base import Task
 from distilabel.tasks.prompt import Prompt
 from distilabel.tasks.text_generation.principles import UltraFeedbackPrinciples
+from distilabel.utils.imports import _ARGILLA_AVAILABLE
+
+if _ARGILLA_AVAILABLE:
+    import argilla as rg
+
+if TYPE_CHECKING:
+    from argilla.client.feedback.dataset.local.dataset import FeedbackDataset
+    from argilla.client.feedback.schemas.records import FeedbackRecord
 
 
 @dataclass
@@ -135,3 +145,91 @@ class TextGenerationTask(Task):
     def output_args_names(self) -> list[str]:
         """Returns the output args names for the task."""
         return ["generations"]
+
+    def to_argilla_dataset(
+        self,
+        dataset_row: Dict[str, Any],
+        generations_column: Optional[str] = "generations",
+    ) -> "FeedbackDataset":
+        # First we infer the fields from the input_args_names, but we could also
+        # create those manually instead using `rg.TextField(...)`
+        fields = infer_fields_from_dataset_row(
+            field_names=self.input_args_names + self.output_args_names,
+            dataset_row=dataset_row,
+        )
+        # Then we add a default `RatingQuestion` which asks the users to provide a
+        # rating for each of the generations, differing from the scenario where the inputs
+        # are the fields and the outputs the ones used to formulate the quesstions. So on,
+        # in this scenario we won't have suggestions, as the questions will be related to the
+        # combination of inputs and outputs.
+        if generations_column is None or generations_column not in dataset_row:
+            raise ValueError(
+                f"The `generations_column='{generations_column}'` is not present in the dataset"
+                f" row. Please provide any of {list(dataset_row.keys())}.",
+            )
+        questions = []
+        for idx in range(1, len(dataset_row[generations_column]) + 1):
+            questions.append(
+                rg.RatingQuestion(  # type: ignore
+                    name=f"{generations_column}-{idx}-rating",
+                    title=f"How would you rate the generation at `{generations_column}-{idx}`?",
+                    values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                )
+            )
+        # Finally, we define some metadata properties that can be potentially used
+        # while exploring the dataset within Argilla to get more insights on the data.
+        metadata_properties = []
+        for arg_name in self.input_args_names + self.output_args_names:
+            if isinstance(dataset_row[arg_name], list):
+                for idx in range(1, len(dataset_row[arg_name]) + 1):
+                    metadata_properties.append(
+                        rg.IntegerMetadataProperty(name=f"length-{arg_name}-{idx}")  # type: ignore
+                    )
+            elif isinstance(dataset_row[arg_name], str):
+                metadata_properties.append(
+                    rg.IntegerMetadataProperty(name=f"length-{arg_name}")  # type: ignore
+                )
+            else:
+                warnings.warn(
+                    f"Unsupported input type ({type(dataset_row[arg_name])}), skipping...",
+                    UserWarning,
+                    stacklevel=2,
+                )
+        # Then we just return the `FeedbackDataset` with the fields, questions, and metadata properties
+        # defined above.
+        return rg.FeedbackDataset(
+            fields=fields,
+            questions=questions,
+            metadata_properties=metadata_properties,  # Note that these are always optional
+        )
+
+    def to_argilla_record(self, dataset_row: Dict[str, Any]) -> "FeedbackRecord":
+        """Converts a dataset row to an Argilla `FeedbackRecord`."""
+        # We start off with the fields, which are the inputs of the LLM, but also
+        # build the metadata from them, as previously specified within the
+        fields, metadata = {}, {}
+        for arg_name in self.input_args_names + self.output_args_names:
+            arg_value = dataset_row[arg_name]
+            if isinstance(arg_value, list):
+                for idx, value in enumerate(arg_value, start=1):
+                    value = (
+                        value.strip()
+                        if isinstance(value, str)
+                        else "\n".join(value)
+                        if isinstance(value, list)
+                        else ""
+                    )
+                    fields[f"{arg_name}-{idx}"] = value
+                    if value is not None:
+                        metadata[f"length-{arg_name}-{idx}"] = len(value)
+            elif isinstance(arg_value, str):
+                fields[arg_name] = arg_value.strip() if arg_value else ""
+                if arg_value is not None:
+                    metadata[f"length-{arg_name}"] = len(arg_value.strip())
+            else:
+                warnings.warn(
+                    f"Unsupported input type ({type(arg_value)}), skipping...",
+                    UserWarning,
+                    stacklevel=2,
+                )
+        return rg.FeedbackRecord(fields=fields, metadata=metadata)

src/distilabel/tasks/text_generation/self_instruct.py
@@ -43,7 +43,8 @@ class SelfInstructTask(TextGenerationTask):
 
     system_prompt: str = (
         "You are an expert prompt writer, writing the best and most diverse prompts for a variety of tasks."
-        "You are given a task description and a set of instructions for how to write the prompts for a specific AI application."
+        " You are given a task description and a set of instructions for how to write the prompts for an"
+        " specific AI application."
     )
     application_description: str = "AI assistant"
     num_instructions: int = 5