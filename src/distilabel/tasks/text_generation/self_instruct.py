17db4d659083781d86011b68bf3d2ea5fcd726a3
Alvaro Bartolome <alvaro@argilla.io>
2023-12-20 12:41:56 +0100

    Update `pattern` to be more consistent with spacing
    Co-authored-by: Ignacio Talavera <ignacio@argilla.io>

--- src/distilabel/tasks/text_generation/self_instruct.py
+++ src/distilabel/tasks/text_generation/self_instruct.py
@@ -96,7 +96,7 @@ class SelfInstructTask(TextGenerationTask):
 
     def parse_output(self, output: str) -> Dict[str, List[str]]:
         """Parses the output of the model into the desired format."""
-        pattern = re.compile(r"\d+\.\s+(.*?)\n")
+        pattern = re.compile(r"\d+\.\s*(.*?)\n")
         return {"instructions": pattern.findall(output)}
 
     def to_argilla_dataset(self, dataset_row: Dict[str, Any]) -> "FeedbackDataset":
