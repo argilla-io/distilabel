ef6615e243e9b759b5db9cc763fc52ac8b708d70
plaguss <agustin@argilla.io>
2024-01-05 17:19:32 +0100

    Add test for generate method

--- tests/llm/test_anyscale.py
+++ tests/llm/test_anyscale.py
@@ -47,3 +47,50 @@ class TestAnyscaleLLM:
                 task=TextGenerationTask(),
                 api_key="OPENAI_API_KEY",
             )
+
+    def test_generate(self, mock_anyscale):
+        llm = AnyscaleLLM(
+            model="HuggingFaceH4/zephyr-7b-beta",
+            task=TextGenerationTask(),
+            api_key="OPENAI_API_KEY",
+        )
+
+        input = "What is Anyscale?"
+        model_response = "model response"
+        contents = [
+            [
+                {
+                    "model_name": llm.model_name,
+                    "prompt_used": [
+                        {
+                            "role": "system",
+                            "content": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+                        },
+                        {"role": "user", "content": input},
+                    ],
+                    "raw_output": model_response,
+                    "parsed_output": {"generations": model_response},
+                }
+            ]
+        ]
+
+        class MockContent:
+            def __init__(self, content=model_response):
+                self.content = content
+
+        class MockMessage:
+            def __init__(self, message=MockContent()):
+                self.message = message
+
+        choices = [MockMessage()]
+
+        class MockCompletions:
+            def __init__(self, choices=choices):
+                self.choices = choices
+
+        llm.client.chat.completions.create = mock.MagicMock(
+            return_value=MockCompletions()
+        )
+
+        result = llm.generate([{"input": input}])
+        assert result == contents
