f16d14da10d715bafdba2af1972a17ac2b24a581
davidberenstein1957 <davidberenstein1957@users.noreply.github.com>
2024-01-18 14:26:25 +0000

    Deployed cb55007 to dev with MkDocs 1.5.3 and mike 2.0.0

--- dev/snippets/technical-reference/tasks/generic_openai_self_instruct.py
+++ dev/snippets/technical-reference/tasks/generic_openai_self_instruct.py
@@ -8,6 +8,7 @@ generator = OpenAILLM(
         system_prompt="You are a question-answering assistant for...",
         application_description="AI assistant",
         num_instructions=3,
+        criteria_for_query_generation="Design queries to be... ",
     ),
     openai_api_key=os.getenv("OPENAI_API_KEY"),
 )
