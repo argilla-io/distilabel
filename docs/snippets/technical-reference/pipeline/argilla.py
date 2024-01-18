50129d0ce88d886cc0f4a2d94a193ee92073b0d1
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-01-18 16:02:34 +0100

    docs: update docs

--- docs/snippets/technical-reference/pipeline/argilla.py
+++ docs/snippets/technical-reference/pipeline/argilla.py
@@ -2,6 +2,9 @@ import argilla as rg
 from argilla.client.feedback.integrations.sentencetransformers import (
     SentenceTransformersExtractor,
 )
+from argilla.client.feedback.integrations.textdescriptives import (
+    TextDescriptivesExtractor,
+)
 
 rg.init(api_key="<YOUR_ARGILLA_API_KEY>", api_url="<YOUR_ARGILLA_API_URL>")
 
@@ -11,3 +14,7 @@ rg_dataset.push_to_argilla(name="preference-dataset", workspace="admin")
 # with a custom `vector_strategy``
 vector_strategy = SentenceTransformersExtractor(model="TaylorAI/bge-micro-v2")
 rg_dataset = pipe_dataset.to_argilla(vector_strategy=vector_strategy)
+
+# with a custom `metric_strategy`
+metric_strategy = TextDescriptivesExtractor(model="en_core_web_sm")
+rg_dataset = pipe_dataset.to_argilla(metric_strategy=vector_strategy)
