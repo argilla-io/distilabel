42f1a6b84c1cf545ad8be5acec91aa66dd96148a
Ben Burtenshaw <ben@argilla.io>
2024-02-06 14:19:11 +0100

    refactor tests to mock out OpenAI client

--- tests/llm/test_openai.py
+++ tests/llm/test_openai.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import json
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 import pytest
 from distilabel.llm.openai import JSONOpenAILLM
@@ -22,30 +22,42 @@ from distilabel.tasks import TextGenerationTask
 
 @pytest.fixture(scope="module", autouse=True)
 def mock_json_openai_llm():
+    # Mocking available models and models that support JSON
+    json_supporting_model = "gpt-3.5-turbo-1106"
+    available_models = [json_supporting_model]
+    JSONOpenAILLM.available_models = available_models
+    JSONOpenAILLM._available_models = Mock(return_value=available_models)
+
+    # Mocking the OpenAI client and chat method
     mocked_chat_return_value = Mock(
         choices=[Mock(message=Mock(content='{"answer": "Madrid"}'))]
     )
-    with patch(
-        "openai.resources.chat.Completions.create",
-        return_value=mocked_chat_return_value,
-    ):
-        JSONOpenAILLM._available_models = Mock(return_value=["gpt-3.5-turbo-1106"])
-        JSONOpenAILLM.available_models = ["gpt-3.5-turbo-1106"]
-        yield JSONOpenAILLM(
-            model="gpt-3.5-turbo-1106",
-            task=TextGenerationTask(),
-        )
+    mocked_openai_client = Mock(
+        chat=Mock(completions=Mock(create=Mock(return_value=mocked_chat_return_value))),
+        api_key="api_key",
+        organization="organization",
+        models=Mock(list=Mock(return_value=available_models)),
+    )
+    text_generation_task = TextGenerationTask()
+    yield JSONOpenAILLM(
+        model=json_supporting_model,
+        task=text_generation_task,
+        client=mocked_openai_client,
+    )
 
 
 def test_generate(mock_json_openai_llm):
-    prompt = "write a json object with a key 'answer' and value 'Paris'"
-    inputs = [{"input": prompt}]
+    # Setup inputs
+    _prompt = "write a json object with a key 'answer' and value 'Paris'"
+    inputs = [{"input": _prompt}]
     outputs = mock_json_openai_llm.generate(inputs)
+    # Asserts
     assert len(outputs) == 1
     assert isinstance(outputs[0], list)
     json_response = json.loads(outputs[0][0]["parsed_output"]["generations"])
     assert isinstance(json_response, dict)
     assert json_response["answer"] == "Madrid"
+    mock_json_openai_llm.client.chat.completions.create.assert_called_once()
 
 
 def test_available_models(mock_json_openai_llm):
