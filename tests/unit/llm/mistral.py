argilla-io/distilabel

commit 927dc421f778fa34d8d0da11718cbfcf4989b2e4

Update serde (#382)
* Remove duplicate `serialization` module

* Update serialization format and include runtime parameters info

* Update `DAG` deserialization

* Add `description` for `RuntimeParameter` attributes

* Update serde unit tests

* Fix import

* Make Python 3.8 happy

* New keywords

pyproject.toml
@@ -8,7 +8,7 @@ description = "AI Feedback (AIF) framework"
 readme = "README.md"
 requires-python = ">=3.8"
 license = "MIT"
-keywords = ["llm", "annotation", "alignment"]
+keywords = ["llm", "annotation", "alignment", "synthetic", "data", "rlaif"]
 authors = [{ name = "Argilla", email = "admin@argilla.io" }]
 classifiers = [
     "Development Status :: 4 - Beta",

src/distilabel/pipeline/step/base.py
@@ -23,8 +23,8 @@ from typing_extensions import Annotated, get_args, get_origin
 
 from distilabel.pipeline.base import BasePipeline, _GlobalPipelineManager
 from distilabel.pipeline.logging import get_logger
-from distilabel.pipeline.serialization import _Serializable
 from distilabel.pipeline.step.typing import StepInput
+from distilabel.utils.serialization import TYPE_INFO_KEY, _Serializable
 
 if TYPE_CHECKING:
     from pydantic.fields import FieldInfo
@@ -314,33 +314,53 @@ class _Step(BaseModel, _Serializable, ABC):
             It's intended for internal use.
 
         Args:
-            data (Dict[str, Any]): Dict containing the serialized data from a Step and the
-                Pipeline it belongs to.
+            data: dictionary containing the serialized data from a `Step` and the
+                `Pipeline` it belongs to.
 
         Returns:
-            step (Step): Instance of the Step.
+            A `Step` instance.
         """
-        if not (pipe := _GlobalPipelineManager.get_pipeline()):
-            raise ValueError("A Step must be initialized in the context of a Pipeline.")
-
-        # Remove the _type_info_ to avoid errors on instantiation
+        # Remove the "type_info" to avoid errors on instantiation
         _data = data.copy()
-        if "_type_info_" in _data.keys():
-            _data.pop("_type_info_")
+        if TYPE_INFO_KEY in _data.keys():
+            _data.pop(TYPE_INFO_KEY)
 
-        # Before passing the data to instantiate the general step, we have to instantiate some of the internal objects.
-        # For the moment we only take into account the LLM, we should take care if we update any of the objects.
+        # Before passing the data to instantiate the general step, we have to instantiate
+        # some of the internal objects. For the moment we only take into account the LLM,
+        # we should take care if we update any of the objects.
         if llm := _data.get("llm"):
             from distilabel.utils.serialization import _get_class
 
-            nested_cls = _get_class(**llm.pop("_type_info_"))
+            nested_cls = _get_class(**llm.pop(TYPE_INFO_KEY))
             # Load the LLM and update the _data inplace
             nested_cls = nested_cls(**llm)
             _data.update({"llm": nested_cls})
         # Every step needs the pipeline, and the remaining arguments are general
-        step = cls(pipeline=pipe, **_data)
+        step = cls(**_data)
 
         return step
+
+    def _model_dump(self, obj: Any, **kwargs: Any) -> Dict[str, Any]:
+        dump = super()._model_dump(obj, **kwargs)
+        dump["runtime_parameters_info"] = self._get_runtime_parameters_info()
+        return dump
+
+    def _get_runtime_parameters_info(self) -> List[Dict[str, Any]]:
+        """Gets the information of the runtime parameters of the step such as the name and
+        the description. This function is meant to include the information of the runtime
+        parameters in the serialized data of the step.
+
+        Returns:
+            A list containing the information for each runtime parameter of the step.
+        """
+        runtime_parameters_info = []
+        for name, field_info in self.model_fields.items():
+            if name in self.runtime_parameters_names:
+                info = {"name": name, "optional": self.runtime_parameters_names[name]}
+                if field_info.description is not None:
+                    info["description"] = field_info.description
+                runtime_parameters_info.append(info)
+        return runtime_parameters_info
 
 
 class Step(_Step, ABC):

src/distilabel/pipeline/step/generators/huggingface.py
@@ -17,6 +17,7 @@ from typing import Any, Dict, List, Optional, Union
 
 import requests
 from datasets import load_dataset
+from pydantic import Field
 
 from distilabel.pipeline.step.base import GeneratorStep, RuntimeParameter
 from distilabel.pipeline.step.typing import GeneratorStepOutput
@@ -72,14 +73,23 @@ class LoadHubDataset(GeneratorStep):
     - `output`: dynamic, based on the dataset being loaded.
     """
 
-    repo_id: RuntimeParameter[str] = None
-    split: RuntimeParameter[str] = None
-    config: Optional[RuntimeParameter[str]] = None
+    repo_id: RuntimeParameter[str] = Field(
+        default=None,
+        description="The Hugging Face Hub repository ID of the dataset to load.",
+    )
+    split: RuntimeParameter[str] = Field(
+        default=None, description="The split of the dataset to load."
+    )
+    config: Optional[RuntimeParameter[str]] = Field(
+        default=None,
+        description="The configuration of the dataset to load. This is optional and only"
+        " needed if the dataset has multiple configurations.",
+    )
 
     def load(self) -> None:
         """Load the dataset from the Hugging Face Hub"""
         self._values["dataset"] = load_dataset(
-            self.repo_id,
+            self.repo_id,  # type: ignore
             self.config,
             split=self.split,
             streaming=True,

src/distilabel/pipeline/step/globals/huggingface.py
@@ -17,22 +17,40 @@ from collections import defaultdict
 from typing import Optional
 
 from datasets import Dataset
+from pydantic import Field
 
 from distilabel.pipeline.step.base import GlobalStep, RuntimeParameter
 from distilabel.pipeline.step.typing import StepInput, StepOutput
 
 
 class PushToHub(GlobalStep):
-    repo_id: RuntimeParameter[str]
-    split: RuntimeParameter[str] = "train"
-    private: RuntimeParameter[bool] = False
-    token: Optional[RuntimeParameter[str]] = None
+    """A `GlobalStep` which creates a `datasets.Dataset` with the input data and pushes
+    it to the Hugging Face Hub."""
+
+    repo_id: RuntimeParameter[str] = Field(
+        default=None,
+        description="The Hugging Face Hub repository ID where the dataset will be uploaded.",
+    )
+    split: RuntimeParameter[str] = Field(
+        default="train",
+        description="The split of the dataset that will be pushed. Defaults to 'train'.",
+    )
+    private: RuntimeParameter[bool] = Field(
+        default=False,
+        description="Whether the dataset to be pushed should be private or not. Defaults"
+        " to `False`.",
+    )
+    token: Optional[RuntimeParameter[str]] = Field(
+        default=None,
+        description="The token that will be used to authenticate in the Hub. If not provided,"
+        " the token will be tried to be obtained from the environment variable `HF_TOKEN`."
+        " If not provided using one of the previous methods, then `huggingface_hub` library"
+        " will try to use the token from the local Hugging Face CLI configuration. Defaults"
+        " to `None`",
+    )
 
     # NOTE: `process` should be able to not return anything i.e. LeafStep, or just return None
-    def process(
-        self,
-        inputs: StepInput,
-    ) -> StepOutput:
+    def process(self, inputs: StepInput) -> StepOutput:
         dataset_dict = defaultdict(list)
         for input in inputs:
             for key, value in input.items():
@@ -40,7 +58,7 @@ class PushToHub(GlobalStep):
         dataset_dict = dict(dataset_dict)
         dataset = Dataset.from_dict(dataset_dict)
         dataset.push_to_hub(
-            self.repo_id,
+            self.repo_id,  # type: ignore
             split=self.split,
             private=self.private,
             token=self.token or os.getenv("HF_TOKEN"),

tests/unit/pipeline/step/test_base.py
@@ -23,6 +23,7 @@ from distilabel.pipeline.step.base import (
     Step,
 )
 from distilabel.pipeline.step.typing import GeneratorStepOutput, StepInput, StepOutput
+from distilabel.utils.serialization import TYPE_INFO_KEY
 
 
 class DummyStep(Step):
@@ -218,7 +219,7 @@ class TestGlobalStep:
 
 
 class TestStepSerialization:
-    def test_step_dump(self):
+    def test_step_dump(self) -> None:
         pipeline = Pipeline()
         step = DummyStep(name="dummy", pipeline=pipeline)
         assert step.dump() == {
@@ -226,45 +227,39 @@ class TestStepSerialization:
             "input_batch_size": 50,
             "input_mappings": {},
             "output_mappings": {},
-            "_type_info_": {
+            "runtime_parameters_info": [],
+            TYPE_INFO_KEY: {
                 "module": "tests.unit.pipeline.step.test_base",
                 "name": "DummyStep",
             },
         }
 
-    def test_step_from_dict(self):
-        with Pipeline() as pipe:
-            assert isinstance(
-                DummyStep.from_dict(
-                    {
-                        **{
-                            "name": "dummy",
-                            "_type_info_": {
-                                "module": "tests.unit.pipeline.step.test_base",
-                                "name": "DummyStep",
-                            },
+    def test_step_from_dict(self) -> None:
+        with Pipeline():
+            step = DummyStep.from_dict(
+                {
+                    **{
+                        "name": "dummy",
+                        TYPE_INFO_KEY: {
+                            "module": "tests.unit.pipeline.step.test_base",
+                            "name": "DummyStep",
                         },
-                        **pipe.dump(),
                     }
-                ),
-                DummyStep,
+                }
             )
 
-    def test_step_from_dict_without_pipeline_context(self):
-        pipe = Pipeline()
+        assert isinstance(step, DummyStep)
+
+    def test_step_from_dict_without_pipeline_context(self) -> None:
         with pytest.raises(ValueError):
-            assert isinstance(
-                DummyStep.from_dict(
-                    {
-                        **{
-                            "name": "dummy",
-                            "_type_info_": {
-                                "module": "tests.pipeline.step.test_base",
-                                "name": "DummyStep",
-                            },
+            DummyStep.from_dict(
+                {
+                    **{
+                        "name": "dummy",
+                        TYPE_INFO_KEY: {
+                            "module": "tests.pipeline.step.test_base",
+                            "name": "DummyStep",
                         },
-                        **pipe.dump(),
                     }
-                ),
-                DummyStep,
+                }
             )

commit 9384dfd0cdbf703dd3336013416d821030a12d54

Add `MistralLLM` and add `generation_kwargs` as `RuntimeParameters` (#383)
* Move `steps/task/combine.py` to `steps/combine.py` as not a `Task`

* Add `MistralLLM`

* Add missing `# type: ignore`

* Bump `mistralai` dependency to `>=0.1.0`

* Move `RuntimeParameter` to `step/typing.py`

* Add `generation_kwargs` as `RuntimeParameter` in `Task`

* Fix `test_serialization` bug introduced in #382

* Add `TestMistralLLM`

* Skip `TestMistralLLM` if Python < 3.9

* Update `test.yml` to skip `mistralai` installation if `python3.8`

* Fix `python_version` check in `test.yml`

.github/workflows/test.yml
@@ -41,7 +41,15 @@ jobs:
 
       - name: Install dependencies
         if: steps.cache.outputs.cache-hit != 'true'
-        run: pip install -e .[dev,tests,hf-transformers,hf-inference-endpoints,vertexai,ollama,openai,together,argilla,mistralai]
+        run: |
+          python_version=$(python -c "import sys; print(sys.version_info[:2])")
+
+          if [ "${python_version}" != "(3, 8)" ]; then
+            pip install -e .[dev,tests,hf-transformers,hf-inference-endpoints,vertexai,ollama,openai,together,argilla,mistralai]
+          else
+            pip install -e .[dev,tests,hf-transformers,hf-inference-endpoints,vertexai,ollama,openai,together,argilla]
+          fi
+
       - name: Lint
         run: make lint
 

pyproject.toml
@@ -44,7 +44,7 @@ vertexai = ["google-cloud-aiplatform >= 1.38.0"]
 together = ["together"]
 argilla = ["argilla >= 1.23.0"]
 tests = ["pytest >= 7.4.0", "pytest-asyncio", "nest-asyncio"]
-mistralai = ["mistralai >= 0.0.11"]
+mistralai = ["mistralai >= 0.1.0"]
 docs = [
     "mkdocs-material >= 9.5.0",
     "mkdocstrings[python] >= 0.24.0",

src/distilabel/llm/mistral.py
@@ -1,0 +1,91 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from typing import TYPE_CHECKING, Optional, Union
+
+from mistralai.async_client import MistralAsyncClient
+from pydantic import Field, PrivateAttr, SecretStr, field_validator
+from typing_extensions import Annotated
+
+from distilabel.llm.base import AsyncLLM
+
+if TYPE_CHECKING:
+    from distilabel.pipeline.step.task.typing import ChatType
+
+
+class MistralLLM(AsyncLLM):
+    """Mistral LLM implementation running the Async API client.
+
+    Args:
+        model: the model name to use for the LLM e.g. "mistral-tiny", "mistral-large", etc.
+        api_key: the API key to authenticate the requests to the Mistral API.
+
+    """
+
+    api_key: Annotated[Optional[SecretStr], Field(validate_default=True)] = None
+    model: str = "mistral-medium"
+    endpoint: str = "https://api.mistral.ai"
+    max_retries: int = 5
+    timeout: int = 120
+    max_concurrent_requests: int = 64
+
+    _aclient: Optional["MistralAsyncClient"] = PrivateAttr(...)
+
+    @field_validator("api_key")
+    @classmethod
+    def api_key_must_not_be_none(cls, v: Union[str, SecretStr, None]) -> SecretStr:
+        """Ensures that either the `api_key` or the environment variable `MISTRAL_API_KEY` are set.
+
+        Additionally, the `api_key` when provided is casted to `pydantic.SecretStr` to prevent it
+        from being leaked and/or included within the logs or the serialization of the object.
+        """
+        v = v or os.getenv("MISTRAL_API_KEY", None)  # type: ignore
+        if v is None:
+            raise ValueError("You must provide an API key to use Mistral.")
+        if not isinstance(v, SecretStr):
+            v = SecretStr(v)
+        return v
+
+    def load(self) -> None:
+        """Loads the `MistralAsyncClient` client to benefit from async requests."""
+        self._aclient = MistralAsyncClient(
+            api_key=self.api_key.get_secret_value(),  # type: ignore
+            endpoint=self.endpoint,
+            max_retries=self.max_retries,
+            timeout=self.timeout,
+            max_concurrent_requests=self.max_concurrent_requests,
+        )
+
+    @property
+    def model_name(self) -> str:
+        """Returns the model name used for the LLM."""
+        return self.model
+
+    async def agenerate(
+        self,
+        input: "ChatType",
+        temperature: Optional[float] = None,
+        max_tokens: Optional[int] = None,
+        top_p: Optional[float] = None,
+    ) -> str:
+        """Generates a response asynchronously, using the Mistral Async API."""
+        completion = await self._aclient.chat(  # type: ignore
+            messages=input,
+            model=self.model,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+        )
+        return completion.choices[0].message.content  # type: ignore

src/distilabel/pipeline/step/base.py
@@ -16,14 +16,14 @@ import inspect
 import logging
 from abc import ABC, abstractmethod
 from functools import cached_property
-from typing import TYPE_CHECKING, Any, Dict, List, Tuple, TypeVar, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Tuple, Union
 
 from pydantic import BaseModel, ConfigDict, Field, PositiveInt, PrivateAttr
 from typing_extensions import Annotated, get_args, get_origin
 
 from distilabel.pipeline.base import BasePipeline, _GlobalPipelineManager
 from distilabel.pipeline.logging import get_logger
-from distilabel.pipeline.step.typing import StepInput
+from distilabel.pipeline.step.typing import _RUNTIME_PARAMETER_ANNOTATION, StepInput
 from distilabel.utils.serialization import TYPE_INFO_KEY, _Serializable
 
 if TYPE_CHECKING:
@@ -32,16 +32,6 @@ if TYPE_CHECKING:
     from distilabel.pipeline.step.typing import GeneratorStepOutput, StepOutput
 
 DEFAULT_INPUT_BATCH_SIZE = 50
-
-
-_T = TypeVar("_T")
-
-_RUNTIME_PARAMETER_ANNOTATION = "distilabel_step_runtime_parameter"
-
-RuntimeParameter = Annotated[
-    Union[_T, None], Field(default=None), _RUNTIME_PARAMETER_ANNOTATION
-]
-"""Used to mark the attributes of a `Step` as a runtime parameter."""
 
 
 class _Step(BaseModel, _Serializable, ABC):

src/distilabel/pipeline/step/generators/huggingface.py
@@ -19,8 +19,8 @@ import requests
 from datasets import load_dataset
 from pydantic import Field
 
-from distilabel.pipeline.step.base import GeneratorStep, RuntimeParameter
-from distilabel.pipeline.step.typing import GeneratorStepOutput
+from distilabel.pipeline.step.base import GeneratorStep
+from distilabel.pipeline.step.typing import GeneratorStepOutput, RuntimeParameter
 
 
 @lru_cache

src/distilabel/pipeline/step/globals/huggingface.py
@@ -19,8 +19,8 @@ from typing import Optional
 from datasets import Dataset
 from pydantic import Field
 
-from distilabel.pipeline.step.base import GlobalStep, RuntimeParameter
-from distilabel.pipeline.step.typing import StepInput, StepOutput
+from distilabel.pipeline.step.base import GlobalStep
+from distilabel.pipeline.step.typing import RuntimeParameter, StepInput, StepOutput
 
 
 class PushToHub(GlobalStep):

src/distilabel/pipeline/step/task/base.py
@@ -13,16 +13,27 @@
 # limitations under the License.
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict
+from typing import Any, Dict, Optional
+
+from pydantic import Field
 
 from distilabel.llm.base import LLM
 from distilabel.pipeline.step.base import Step
 from distilabel.pipeline.step.task.typing import ChatType
-from distilabel.pipeline.step.typing import StepInput, StepOutput
+from distilabel.pipeline.step.typing import RuntimeParameter, StepInput, StepOutput
 
 
 class Task(Step, ABC):
     llm: LLM
+
+    generation_kwargs: Optional[RuntimeParameter[Dict[str, Any]]] = Field(
+        default_factory=dict,
+        description="The kwargs to be propagated to either `generate` or `agenerate`"
+        " methods within each `LLM`. Note that these kwargs will be specific to each"
+        " LLM, and while some as `temperature` may be present on each `LLM`, some others"
+        " may not, so read the `LLM.{generate,agenerate}` signatures in advance to see"
+        " which kwargs are available.",
+    )
 
     def load(self) -> None:
         self.llm.load()  # type: ignore
@@ -37,13 +48,13 @@ class Task(Step, ABC):
 
     def process(self, inputs: StepInput) -> StepOutput:
         formatted_inputs = [self.format_input(input) for input in inputs]
-        outputs = self.llm.generate(formatted_inputs)  # type: ignore
+        outputs = self.llm.generate(formatted_inputs, **self.generation_kwargs)  # type: ignore
         formatted_outputs = [self.format_output(output) for output in outputs]  # type: ignore
 
-        outputs: StepOutput = []
+        outputs: StepOutput = []  # type: ignore
         for input, formatted_output in zip(inputs, formatted_outputs):
             output = {k: v for k, v in input.items() if k in self.inputs}
             output.update(formatted_output)
             output["model_name"] = self.llm.model_name  # type: ignore
-            outputs.append(output)
+            outputs.append(output)  # type: ignore
         yield outputs  # type: ignore

src/distilabel/pipeline/step/typing.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, Iterator, List, Tuple
+from typing import Any, Dict, Iterator, List, Tuple, TypeVar, Union
 
+from pydantic import Field
 from typing_extensions import Annotated
 
 StepInput = Annotated[List[Dict[str, Any]], "StepInput"]
@@ -28,3 +29,11 @@ GeneratorStepOutput = Annotated[
     Iterator[Tuple[List[Dict[str, Any]], bool]], "GeneratorStepOutput"
 ]
 """GeneratorStepOutput is just an `Annotated` alias of the typing `Iterator[Tuple[List[Dict[str, Any]], bool]]`"""
+
+_T = TypeVar("_T")
+_RUNTIME_PARAMETER_ANNOTATION = "distilabel_step_runtime_parameter"
+
+RuntimeParameter = Annotated[
+    Union[_T, None], Field(default=None), _RUNTIME_PARAMETER_ANNOTATION
+]
+"""Used to mark the attributes of a `Step` as a runtime parameter."""

tests/integration/test_pipe_llms.py
@@ -18,10 +18,10 @@ from typing import Any, Dict, Generator, List
 from distilabel.llm.huggingface.transformers import TransformersLLM
 from distilabel.llm.openai import OpenAILLM
 from distilabel.pipeline.local import Pipeline
-from distilabel.pipeline.step.base import RuntimeParameter, Step
+from distilabel.pipeline.step.base import Step
 from distilabel.pipeline.step.generators.huggingface import LoadHubDataset
 from distilabel.pipeline.step.task.generation import TextGeneration
-from distilabel.pipeline.step.typing import StepInput
+from distilabel.pipeline.step.typing import RuntimeParameter, StepInput
 
 
 class RenameColumns(Step):
@@ -33,13 +33,13 @@ class RenameColumns(Step):
 
     @property
     def outputs(self) -> List[str]:
-        return list(self.rename_mappings.values())
+        return list(self.rename_mappings.values())  # type: ignore
 
     def process(self, inputs: StepInput) -> Generator[List[Dict[str, Any]], None, None]:
         outputs = []
         for input in inputs:
             outputs.append(
-                {self.rename_mappings.get(k, k): v for k, v in input.items()}
+                {self.rename_mappings.get(k, k): v for k, v in input.items()}  # type: ignore
             )
         yield outputs
 

tests/integration/test_pipe_simple.py
@@ -16,9 +16,9 @@ from pathlib import Path
 from typing import Any, Dict, Generator, List
 
 from distilabel.pipeline.local import Pipeline
-from distilabel.pipeline.step.base import RuntimeParameter, Step
+from distilabel.pipeline.step.base import Step
 from distilabel.pipeline.step.generators.huggingface import LoadHubDataset
-from distilabel.pipeline.step.typing import StepInput
+from distilabel.pipeline.step.typing import RuntimeParameter, StepInput
 
 
 class RenameColumns(Step):
@@ -30,13 +30,13 @@ class RenameColumns(Step):
 
     @property
     def outputs(self) -> List[str]:
-        return list(self.rename_mappings.values())
+        return list(self.rename_mappings.values())  # type: ignore
 
     def process(self, inputs: StepInput) -> Generator[List[Dict[str, Any]], None, None]:
         outputs = []
         for input in inputs:
             outputs.append(
-                {self.rename_mappings.get(k, k): v for k, v in input.items()}
+                {self.rename_mappings.get(k, k): v for k, v in input.items()}  # type: ignore
             )
         yield outputs
 
@@ -59,7 +59,7 @@ class GenerateResponse(Step):
 def test_pipeline():
     with Pipeline() as pipeline:
         load_hub_dataset = LoadHubDataset(name="load_dataset")
-        rename_columns = RenameColumns(name="rename_columns")
+        rename_columns = RenameColumns(name="rename_columns")  # type: ignore
         generate_response = GenerateResponse(name="generate_response")
 
         load_hub_dataset.connect(rename_columns)

tests/unit/llm/mistral.py
@@ -13,29 +13,33 @@
 # limitations under the License.
 
 import os
+import sys
 from unittest.mock import AsyncMock, MagicMock, Mock, patch
 
 import nest_asyncio
 import pytest
-from distilabel.llm.openai import OpenAILLM
+from distilabel.llm.mistral import MistralLLM
 
 
-@patch("openai.AsyncOpenAI")
-class TestOpenAILLM:
-    def test_openai_llm(self, mock_openai: MagicMock) -> None:
-        llm = OpenAILLM(model="gpt-4", api_key="api.key")  # type: ignore
-        assert isinstance(llm, OpenAILLM)
-        assert llm.model_name == "gpt-4"
+@pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="`mistralai` requires Python 3.9 or higher"
+)
+@patch("mistralai.async_client.MistralAsyncClient")
+class TestMistralLLM:
+    def test_mistral_llm(self, mock_mistral: MagicMock) -> None:
+        llm = MistralLLM(model="mistral-tiny", api_key="api.key")  # type: ignore
+        assert isinstance(llm, MistralLLM)
+        assert llm.model_name == "mistral-tiny"
 
     @pytest.mark.asyncio
-    async def test_agenerate(self, mock_openai: MagicMock) -> None:
-        llm = OpenAILLM(model="gpt-4", api_key="api.key")  # type: ignore
-        llm._aclient = mock_openai
+    async def test_agenerate(self, mock_mistral: MagicMock) -> None:
+        llm = MistralLLM(model="mistral-tiny", api_key="api.key")  # type: ignore
+        llm._aclient = mock_mistral
 
         mocked_completion = Mock(
             choices=[Mock(message=Mock(content=" Aenean hendrerit aliquam velit. ..."))]
         )
-        llm._aclient.chat.completions.create = AsyncMock(return_value=mocked_completion)
+        llm._aclient.chat = AsyncMock(return_value=mocked_completion)
 
         await llm.agenerate(
             input=[
@@ -48,14 +52,14 @@ class TestOpenAILLM:
         )
 
     @pytest.mark.asyncio
-    async def test_generate(self, mock_openai: MagicMock) -> None:
-        llm = OpenAILLM(model="gpt-4", api_key="api.key")  # type: ignore
-        llm._aclient = mock_openai
+    async def test_generate(self, mock_mistral: MagicMock) -> None:
+        llm = MistralLLM(model="mistral-tiny", api_key="api.key")  # type: ignore
+        llm._aclient = mock_mistral
 
         mocked_completion = Mock(
             choices=[Mock(message=Mock(content=" Aenean hendrerit aliquam velit. ..."))]
         )
-        llm._aclient.chat.completions.create = AsyncMock(return_value=mocked_completion)
+        llm._aclient.chat = AsyncMock(return_value=mocked_completion)
 
         nest_asyncio.apply()
 
@@ -71,17 +75,21 @@ class TestOpenAILLM:
             ]
         )
 
-    def test_serialization(self, mock_openai: MagicMock) -> None:
-        os.environ["OPENAI_API_KEY"] = "api.key"
-        llm = OpenAILLM(model="gpt-4")  # type: ignore
+    def test_serialization(self, mock_mistral: MagicMock) -> None:
+        os.environ["MISTRAL_API_KEY"] = "api.key"
+        llm = MistralLLM(model="mistral-tiny")  # type: ignore
 
         _dump = {
-            "model": "gpt-4",
+            "model": "mistral-tiny",
+            "endpoint": "https://api.mistral.ai",
+            "max_retries": 5,
+            "timeout": 120,
+            "max_concurrent_requests": 64,
             "type_info": {
-                "module": "distilabel.llm.openai",
-                "name": "OpenAILLM",
+                "module": "distilabel.llm.mistral",
+                "name": "MistralLLM",
             },
         }
 
         assert llm.dump() == _dump
-        assert isinstance(OpenAILLM.from_dict(_dump), OpenAILLM)
+        assert isinstance(MistralLLM.from_dict(_dump), MistralLLM)

tests/unit/pipeline/step/test_base.py
@@ -19,10 +19,14 @@ from distilabel.pipeline.local import Pipeline
 from distilabel.pipeline.step.base import (
     GeneratorStep,
     GlobalStep,
-    RuntimeParameter,
     Step,
 )
-from distilabel.pipeline.step.typing import GeneratorStepOutput, StepInput, StepOutput
+from distilabel.pipeline.step.typing import (
+    GeneratorStepOutput,
+    RuntimeParameter,
+    StepInput,
+    StepOutput,
+)
 from distilabel.utils.serialization import TYPE_INFO_KEY
 
 

tests/unit/pipeline/test_dag.py
@@ -19,8 +19,8 @@ from typing import TYPE_CHECKING, Any, Callable, Dict, List
 import pytest
 from distilabel.pipeline._dag import DAG
 from distilabel.pipeline.local import Pipeline
-from distilabel.pipeline.step.base import GeneratorStep, RuntimeParameter, Step
-from distilabel.pipeline.step.typing import StepInput
+from distilabel.pipeline.step.base import GeneratorStep, Step
+from distilabel.pipeline.step.typing import RuntimeParameter, StepInput
 
 from .utils import DummyGeneratorStep
 