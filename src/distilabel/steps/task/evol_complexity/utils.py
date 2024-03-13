f860551ed7ce2391fc9ae1d85131c247573030bc
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-03-13 13:26:54 +0100

    chore: initial work on evol_complexity based on WizardLM evol_instruct GH

---
+++ src/distilabel/steps/task/evol_complexity/utils.py
@@ -0,0 +1,44 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+from distilabel.steps.task.evol_instruct.utils import (
+    GenerationMutationTemplates as EvolInstructionGenerationMutationTemplates,
+)
+from distilabel.steps.task.evol_instruct.utils import (
+    MutationTemplates as EvolInstructionMutationTemplates,
+)
+
+if sys.version_info < (3, 11):
+    from enum import Enum as StrEnum
+else:
+    from enum import StrEnum
+
+
+class MutationTemplates(StrEnum):
+    ADD_CONSTRAINTS = EvolInstructionMutationTemplates("ADD_CONSTRAINTS")
+    DEEPEN = EvolInstructionMutationTemplates("DEEPEN")
+    CONCRETIZE = EvolInstructionMutationTemplates("CONCRETIZE")
+    INCREASE_REASONING = EvolInstructionMutationTemplates("INCREASE_REASONING")
+
+
+class GenerationMutationTemplates(StrEnum):
+    FRESH_START = EvolInstructionGenerationMutationTemplates("FRESH_START")
+    ADD_CONSTRAINTS = EvolInstructionGenerationMutationTemplates("ADD_CONSTRAINTS")
+    DEEPEN = EvolInstructionGenerationMutationTemplates("DEEPEN")
+    CONCRETIZE = EvolInstructionGenerationMutationTemplates("CONCRETIZE")
+    INCREASE_REASONING = EvolInstructionGenerationMutationTemplates(
+        "INCREASE_REASONING"
+    )
