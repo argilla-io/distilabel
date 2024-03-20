6cbdf36a115322a0cbcd4bb3b3638283891ff24b
sdiazlor <sdiazlor@gmail.com>
2024-03-20 16:33:37 +0100

    fix: use _

--- tests/unit/llm/test_anthropic.py
+++ tests/unit/llm/test_anthropic.py
@@ -22,7 +22,7 @@ from distilabel.llm.anthropic import AnthropicLLM
 
 @patch("anthropic.AsyncAnthropic")
 class TestAnthropicLLM:
-    def test_anthropic_llm(self, mock_anthropic: MagicMock) -> None:
+    def test_anthropic_llm(self, _: MagicMock) -> None:
         llm = AnthropicLLM(model="claude-3-opus-20240229")  # type: ignore
         assert isinstance(llm, AnthropicLLM)
         assert llm.model_name == "claude-3-opus-20240229"
@@ -69,7 +69,7 @@ class TestAnthropicLLM:
             ],
         )
 
-    def test_serialization(self, mock_anthropic: MagicMock) -> None:
+    def test_serialization(self, _: MagicMock) -> None:
         os.environ["ANTHROPIC_API_KEY"] = "api.key"
         llm = AnthropicLLM(model="claude-3-opus-20240229")  # type: ignore
 
