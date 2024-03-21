c00d74a778d5f7390123f5ac4d0bdfd7a76bcac7
Alvaro Bartolome <alvaro@argilla.io>
2024-03-21 09:20:26 +0100

    Update `base_url` default in `TogetherLLM`

--- src/distilabel/llm/together.py
+++ src/distilabel/llm/together.py
@@ -27,13 +27,15 @@ class TogetherLLM(OpenAILLM):
     Attributes:
         model: the model name to use for the LLM e.g. "mistralai/Mixtral-8x7B-Instruct-v0.1".
             Supported models can be found [here](https://api.together.xyz/models).
-        base_url: the base URL to use for the Together API can be set with `OPENAI_BASE_URL`.
+        base_url: the base URL to use for the Together API can be set with `TOGETHER_BASE_URL`.
             Defaults to "https://api.together.xyz/v1".
         api_key: the API key to authenticate the requests to the Together API. Defaults to the
             value set for the environment variable `TOGETHER_API_KEY`, or `None` if not set.
     """
 
-    base_url: Optional[str] = "https://api.together.xyz/v1"
+    base_url: Optional[str] = os.getenv(
+        "TOGETHER_BASE_URL", "https://api.together.xyz/v1"
+    )
     api_key: Optional[SecretStr] = os.getenv("TOGETHER_API_KEY", None)  # type: ignore
 
     _env_var: Optional[str] = PrivateAttr(default="TOGETHER_API_KEY")
