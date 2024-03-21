62f5f2a02e179af06f9a45009e91d136ad81fb07
Alvaro Bartolome <alvaro@argilla.io>
2024-03-21 19:06:29 +0100

    Increase `InferenceEndpointsLLM` timeout when `paused` or `scaledToZero`

--- src/distilabel/llm/huggingface/inference_endpoints.py
+++ src/distilabel/llm/huggingface/inference_endpoints.py
@@ -150,7 +150,7 @@ class InferenceEndpointsLLM(AsyncLLM):
                 token=self.api_key.get_secret_value(),
             )
             if client.status in ["paused", "scaledToZero"]:
-                client.resume().wait(timeout=30)
+                client.resume().wait(timeout=300)
 
             self.base_url = client.url
             self._model_name = client.repository
