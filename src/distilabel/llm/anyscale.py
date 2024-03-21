503468f9443b3d7b7320678fb129f8ca548f29e4
Alvaro Bartolome <alvaro@argilla.io>
2024-03-21 09:20:16 +0100

    Fix `AnyscaleLLM`

--- src/distilabel/llm/anyscale.py
+++ src/distilabel/llm/anyscale.py
@@ -12,18 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+from typing import Optional
+
+from pydantic import PrivateAttr, SecretStr
 
 from distilabel.llm.openai import OpenAILLM
 
 
 class AnyscaleLLM(OpenAILLM):
-    """
-    Anyscale LLM implementation running the async API client of OpenAI because of duplicate API behavior.
+    """Anyscale LLM implementation running the async API client of OpenAI because of
+    duplicate API behavior.
 
     Attributes:
-        model: the model name to use for the LLM, e.g., `google/gemma-7b-it`. [Supported models](https://docs.endpoints.anyscale.com/text-generation/supported-models/google-gemma-7b-it).
-        base_url: the base URL to use for the Anyscale API can be set with `OPENAI_BASE_URL`. Default is "https://api.endpoints.anyscale.com/v1".
-        api_key: the API key to authenticate the requests to the Anyscale API. Can be set with `OPENAI_API_KEY`. Default is `None`.
+        model: the model name to use for the LLM, e.g., `google/gemma-7b-it`. See the
+            supported models under the "Text Generation -> Supported Models" section
+            [here](https://docs.endpoints.anyscale.com/).
+        base_url: the base URL to use for the Anyscale API can be set with `ANYSCALE_BASE_URL`.
+            Defaults to the value set for the environment variable `ANYSCALE_BASE_URL`, or
+            "https://api.endpoints.anyscale.com/v1" if not set.
+        api_key: the API key to authenticate the requests to the Anyscale API. Defaults to the
+            value set for the environment variable `ANYSCALE_API_KEY`, or `None` if not set.
+
     """
 
-    base_url: str = "https://api.endpoints.anyscale.com/v1"
+    base_url: Optional[str] = os.getenv(
+        "ANYSCALE_BASE_URL", "https://api.endpoints.anyscale.com/v1"
+    )
+    api_key: Optional[SecretStr] = os.getenv("ANYSCALE_API_KEY", None)  # type: ignore
+
+    _env_var: Optional[str] = PrivateAttr(default="ANYSCALE_API_KEY")
