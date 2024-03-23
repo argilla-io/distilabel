a7b739716103816dedb85665ed19b0564f3a7780
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-03-23 15:15:24 +0100

    Fix import

--- src/distilabel/steps/task/evol_quality/base.py
+++ src/distilabel/steps/task/evol_quality/base.py
@@ -25,7 +25,7 @@ import numpy as np
 from pydantic import Field
 from typing_extensions import override
 
-from distilabel.steps.base import RuntimeParameter
+from distilabel.mixins.runtime_parameters import RuntimeParameter
 from distilabel.steps.task.base import Task
 from distilabel.steps.task.evol_quality.utils import MutationTemplates
 from distilabel.steps.task.typing import ChatType
