2b5dcece8e7cecc14f20d5c0044712077cf84b18
Alvaro Bartolome <alvaro@argilla.io>
2024-04-05 10:54:16 +0200

    Fix `base_url` docstring

--- src/distilabel/llms/azure.py
+++ src/distilabel/llms/azure.py
@@ -36,7 +36,7 @@ class AzureOpenAILLM(OpenAILLM):
         model: the model name to use for the LLM i.e. the name of the Azure deployment.
         base_url: the base URL to use for the Azure OpenAI API can be set with `AZURE_OPENAI_ENDPOINT`.
             Defaults to `None` which means that the value set for the environment variable
-            `TOGETHER_BASE_URL` will be used, or `None` if not set.
+            `AZURE_OPENAI_ENDPOINT` will be used, or `None` if not set.
         api_key: the API key to authenticate the requests to the Azure OpenAI API. Defaults to `None`
             which means that the value set for the environment variable `AZURE_OPENAI_API_KEY` will be
             used, or `None` if not set.
