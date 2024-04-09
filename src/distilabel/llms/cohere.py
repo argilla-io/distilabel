62caee04b496817b1d035d8b4368820ad2d5fd08
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-04-09 17:40:32 +0200

    Add `CohereLLM` (#508)
    * Draft refactor docs
    
    * Include layout for the api
    
    * Layout for the docs
    
    * Redirect imports of LLMs
    
    * Draft overview and getting started
    
    * Update docstrings
    
    * Fix docstrings
    
    * Fix argilla reference
    
    * Remove extra line-break
    
    * Refactor and rename `llm` -> `llms`
    
    * Refactor and rename `task` -> `tasks`
    
    * Remove extra line-breaks
    
    * Add missing `type: ignore`
    
    * Update `tasks` and `llms` imports
    
    * Fix imports in `tests/`
    
    * Fix `QualityScorer.format_input` signature
    
    * Update `extra.md`
    
    * Fix `mkdocs.yml` API reference for LLMs
    
    * Add `docs/papers` (WIP)
    
    * Update `docs/papers` (WIP)
    
    * Fix imports after rename to `tasks`
    
    * Remove not used files
    
    * Update main page
    
    * Move argilla docs
    
    * Move papers to sections
    
    * Remove old tutorials
    
    * Update nav
    
    * Remove navigation
    
    * Advances on docs, learn section (#497)
    
    * Add section for distiset
    
    * Update distiset
    
    * Add sample images for screenshots of pipeline runs
    
    * Remove unused files
    
    * Draft including tutorial and advances steps, work in progress
    
    * Fix minor bugs and add `docs/sections/papers/*.md` (#499)
    
    * Fix `distilabel.steps.tasks` imports in `__init__`
    
    * Fix formatting in `__init__.py`
    
    * Remove `commit_message` from `push_to_hub`
    
    * Add missing `super().load()` to load `logging`
    
    * Fix `outputs` in `UltraFeedback`
    
    * Add `model_post_init` in `Argilla` to supress `warnings`
    
    * Add `docs/sections/papers/ultrafeedback.md`
    
    * Add `docs/sections/papers/instruction_backtranslation.md`
    
    * Fix `tests/unit`
    
    * Add `httpx` under `TYPE_CHECKING`
    
    * Fix `argilla` optional dependency handling
    
    * Revert `AnthropicLLM.http_client` typing and add `httpx` dependency instead
    
    * Apply suggestions from code review
    
    Co-authored-by: David Berenstein <davidberenstein1957@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: David Berenstein <davidberenstein1957@users.noreply.github.com>
    
    * Docs cli (#502)
    
    * Update serialization method of _BatchManager to write each of the inner steps to a file and load them back (#496)
    
    * Update replacing string
    
    * Add guide to the CLI
    
    * Added CLI to api reference a small reference to that from the tutorial
    
    * Docs caching (#500)
    
    * Update serialization method of _BatchManager to write each of the inner steps to a file and load them back (#496)
    
    * Add docstrings to lost argument in Distiset
    
    * Add section for caching in advanced tutorial
    
    * Add `AzureOpenAILLM` (#505)
    
    * Add `AzureOpenAILLM`
    
    * Update `distilabel.llms` imports
    
    * Fix `base_url` env var and add `api_version` env var
    
    * Add `AzureOpenAILLM` to `test_imports`
    
    * Add `TestAzureOpenAILLM`
    
    * Fix `base_url` docstring
    
    * Remove `together` extra and place `tests` extra properly
    
    * Fix extras alphabetic order in `pyproject.toml`
    
    * Update `docs/index.md` and `README.md`
    
    * Add `docs/api/llms/azure.md`
    
    * Docs steps (#503)
    
    * Update layout of steps
    
    * Add step guide and draft of special types of steps
    
    * Add reference for the step decorator
    
    * Include step decorator in the tutorial
    
    * Add intro to the different types of steps
    
    * Add generator steps
    
    * Update general and global steps
    
    * Fix typos
    
    * Missing argilla steps examples in general steps
    
    * Create initial layout for tasks
    
    * Add special tasks
    
    * Add `StepInput` missing import
    
    * Add `CohereLLM`
    
    * Add missing docstring
    
    * Add unit tests for `CohereLLM`
    
    * Add `cohere` extra
    
    * Reference `CohereLLM`
    
    ---------
    
    Co-authored-by: plaguss <agustin@argilla.io>
    Co-authored-by: Alvaro Bartolome <alvaro@argilla.io>
    Co-authored-by: David Berenstein <davidberenstein1957@users.noreply.github.com>

---
+++ src/distilabel/llms/cohere.py
@@ -0,0 +1,243 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import os
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Type,
+    Union,
+)
+
+from pydantic import Field, PrivateAttr, SecretStr
+from typing_extensions import override
+
+from distilabel.llms.base import AsyncLLM
+from distilabel.mixins.runtime_parameters import RuntimeParameter
+from distilabel.utils.itertools import grouper
+
+if TYPE_CHECKING:
+    from cohere import AsyncClient, ChatMessage
+
+    from distilabel.llms.typing import GenerateOutput
+    from distilabel.steps.tasks.typing import ChatType
+
+_COHERE_API_KEY_ENV_VAR_NAME = "COHERE_API_KEY"
+
+
+class CohereLLM(AsyncLLM):
+    """Cohere API implementation using the async client for concurrent text generation.
+
+
+    Attributes:
+        model: the name of the model from the Cohere API to use for the generation.
+        base_url: the base URL to use for the Cohere API requests. Defaults to
+            `"https://api.cohere.ai/v1"`.
+        api_key: the API key to authenticate the requests to the Cohere API. Defaults to
+            the value of the `COHERE_API_KEY` environment variable.
+        timeout: the maximum time in seconds to wait for a response from the API. Defaults
+            to `120`.
+        client_name: the name of the client to use for the API requests. Defaults to
+            `"distilabel"`.
+        _ChatMessage: the `ChatMessage` class from the `cohere` package.
+        _aclient: the `AsyncClient` client from the `cohere` package.
+
+    Runtime parameters:
+        - `base_url`: the base URL to use for the Cohere API requests. Defaults to
+            `"https://api.cohere.ai/v1"`.
+        - `api_key`: the API key to authenticate the requests to the Cohere API. Defaults
+            to the value of the `COHERE_API_KEY` environment variable.
+        - `timeout`: the maximum time in seconds to wait for a response from the API. Defaults
+            to `120`.
+        - `client_name`: the name of the client to use for the API requests. Defaults to
+            `"distilabel"`.
+    """
+
+    model: str
+    base_url: Optional[RuntimeParameter[str]] = Field(
+        default_factory=lambda: os.getenv(
+            "COHERE_BASE_URL", "https://api.cohere.ai/v1"
+        ),
+        description="The base URL to use for the Cohere API requests.",
+    )
+    api_key: Optional[RuntimeParameter[SecretStr]] = Field(
+        default_factory=lambda: os.getenv(_COHERE_API_KEY_ENV_VAR_NAME),
+        description="The API key to authenticate the requests to the Cohere API.",
+    )
+    timeout: RuntimeParameter[int] = Field(
+        default=120,
+        description="The maximum time in seconds to wait for a response from the API.",
+    )
+    client_name: RuntimeParameter[str] = Field(
+        default="distilabel",
+        description="The name of the client to use for the API requests.",
+    )
+
+    _ChatMessage: Type["ChatMessage"] = PrivateAttr(...)
+    _aclient: "AsyncClient" = PrivateAttr(...)
+
+    @property
+    def model_name(self) -> str:
+        """Returns the model name used for the LLM."""
+        return self.model
+
+    def load(self) -> None:
+        """Loads the `AsyncClient` client from the `cohere` package."""
+
+        super().load()
+
+        try:
+            from cohere import AsyncClient, ChatMessage
+        except ImportError as ie:
+            raise ImportError(
+                "The `cohere` package is required to use the `CohereLLM` class."
+            ) from ie
+
+        self._ChatMessage = ChatMessage
+
+        self._aclient = AsyncClient(
+            api_key=self.api_key.get_secret_value(),  # type: ignore
+            client_name=self.client_name,
+            base_url=self.base_url,
+            timeout=self.timeout,
+        )
+
+    def _format_chat_to_cohere(
+        self, input: "ChatType"
+    ) -> Tuple[Union[str, None], List["ChatMessage"], str]:
+        """Formats the chat input to the Cohere Chat API conversational format.
+
+        Args:
+            input: The chat input to format.
+
+        Returns:
+            A tuple containing the system, chat history, and message.
+        """
+        system = None
+        message = None
+        chat_history = []
+        for item in input:
+            role = item["role"]
+            content = item["content"]
+            if role == "system":
+                system = content
+            elif role == "user":
+                message = content
+            elif role == "assistant":
+                if message is None:
+                    raise ValueError(
+                        "An assistant message but be preceded by a user message."
+                    )
+                chat_history.append(self._ChatMessage(role="USER", message=message))  # type: ignore
+                chat_history.append(self._ChatMessage(role="CHATBOT", message=content))
+                message = None
+
+        if message is None:
+            raise ValueError("The chat input must end with a user message.")
+
+        return system, chat_history, message
+
+    @override
+    async def agenerate(  # type: ignore
+        self,
+        input: "ChatType",
+        temperature: Optional[float] = None,
+        max_tokens: Optional[int] = None,
+        k: Optional[int] = None,
+        p: Optional[float] = None,
+        seed: Optional[float] = None,
+        stop_sequences: Optional[Sequence[str]] = None,
+        frequency_penalty: Optional[float] = None,
+        presence_penalty: Optional[float] = None,
+        raw_prompting: Optional[bool] = None,
+    ) -> Union[str, None]:
+        """Generates a response from the LLM given an input.
+
+        Args:
+            input: a single input in chat format to generate responses for.
+            temperature: the temperature to use for the generation. Defaults to `None`.
+            max_tokens: the maximum number of new tokens that the model will generate.
+                Defaults to `None`.
+            k: the number of highest probability vocabulary tokens to keep for the generation.
+                Defaults to `None`.
+            p: the nucleus sampling probability to use for the generation. Defaults to
+                `None`.
+            seed: the seed to use for the generation. Defaults to `None`.
+            stop_sequences: a list of sequences to use as stopping criteria for the generation.
+                Defaults to `None`.
+            frequency_penalty: the frequency penalty to use for the generation. Defaults
+                to `None`.
+            presence_penalty: the presence penalty to use for the generation. Defaults to
+                `None`.
+            raw_prompting: a flag to use raw prompting for the generation. Defaults to
+                `None`.
+
+        Returns:
+            The generated response from the Cohere API model.
+        """
+        system, chat_history, message = self._format_chat_to_cohere(input)
+
+        response = await self._aclient.chat(  # type: ignore
+            message=message,
+            model=self.model,
+            preamble=system,
+            chat_history=chat_history,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            k=k,
+            p=p,
+            seed=seed,
+            stop_sequences=stop_sequences,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            raw_prompting=raw_prompting,
+        )
+
+        if (text := response.text) == "":
+            self._logger.warning(
+                f"Received no response using Cohere client (model: '{self.model}')."
+                f" Finish reason was: {response.finish_reason}"
+            )
+            return None
+
+        return text
+
+    @override
+    def generate(
+        self,
+        inputs: List["ChatType"],
+        num_generations: int = 1,
+        **kwargs: Any,
+    ) -> List["GenerateOutput"]:
+        """Method to generate a list of responses asynchronously, returning the output
+        synchronously awaiting for the response of each input sent to `agenerate`."""
+
+        async def agenerate(
+            inputs: List["ChatType"], **kwargs: Any
+        ) -> "GenerateOutput":
+            """Internal function to parallelize the asynchronous generation of responses."""
+            tasks = [
+                asyncio.create_task(self.agenerate(input=input, **kwargs))
+                for input in inputs
+                for _ in range(num_generations)
+            ]
+            return await asyncio.gather(*tasks)
+
+        outputs = self.event_loop.run_until_complete(agenerate(inputs, **kwargs))
+        return list(grouper(outputs, n=num_generations, incomplete="ignore"))
---
+++ tests/unit/llms/test_cohere.py
@@ -0,0 +1,111 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from unittest import mock
+
+import nest_asyncio
+import pytest
+from distilabel.llms.cohere import CohereLLM
+
+
+@mock.patch("cohere.AsyncClient")
+class TestCohereLLM:
+    def test_cohere_llm(self, _: mock.MagicMock) -> None:
+        llm = CohereLLM(model="command-r", api_key="api.key")
+
+        assert isinstance(llm, CohereLLM)
+        assert llm.model_name == "command-r"
+
+    def test_cohere_llm_env_vars(self, _: mock.MagicMock) -> None:
+        with mock.patch.dict(os.environ, clear=True):
+            os.environ["COHERE_API_KEY"] = "another.api.key"
+            os.environ["COHERE_BASE_URL"] = "https://example.com"
+
+            llm = CohereLLM(model="command-r")
+
+            assert isinstance(llm, CohereLLM)
+            assert llm.model_name == "command-r"
+            assert llm.base_url == "https://example.com"
+            assert llm.api_key.get_secret_value() == "another.api.key"  # type: ignore
+
+    @pytest.mark.asyncio
+    async def test_agenerate(self, mock_async_client: mock.MagicMock) -> None:
+        llm = CohereLLM(model="command-r")
+        llm._aclient = mock_async_client  # type: ignore
+
+        mocked_completion = mock.Mock(
+            choices=[
+                mock.Mock(
+                    message=mock.Mock(content=" Aenean hendrerit aliquam velit. ...")
+                )
+            ]
+        )
+        llm._aclient.chat = mock.AsyncMock(return_value=mocked_completion)
+
+        await llm.agenerate(
+            input=[
+                {"role": "system", "content": ""},
+                {
+                    "role": "user",
+                    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                },
+            ]
+        )
+
+    @pytest.mark.asyncio
+    async def test_generate(self, mock_async_client: mock.MagicMock) -> None:
+        llm = CohereLLM(model="command-r")
+        llm._aclient = mock_async_client  # type: ignore
+
+        mocked_completion = mock.Mock(
+            choices=[
+                mock.Mock(
+                    message=mock.Mock(content=" Aenean hendrerit aliquam velit. ...")
+                )
+            ]
+        )
+        llm._aclient.chat = mock.AsyncMock(return_value=mocked_completion)
+
+        nest_asyncio.apply()
+
+        llm.generate(
+            inputs=[
+                [
+                    {"role": "system", "content": ""},
+                    {
+                        "role": "user",
+                        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    },
+                ]
+            ]
+        )
+
+    def test_serialization(self, _: mock.MagicMock) -> None:
+        llm = CohereLLM(model="command-r")
+
+        dump = {
+            "model": "command-r",
+            "generation_kwargs": {},
+            "base_url": "https://api.cohere.ai/v1",
+            "timeout": 120,
+            "client_name": "distilabel",
+            "type_info": {
+                "module": "distilabel.llms.cohere",
+                "name": "CohereLLM",
+            },
+        }
+
+        assert llm.dump() == dump
+        assert isinstance(CohereLLM.from_dict(dump), CohereLLM)
