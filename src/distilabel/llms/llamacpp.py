6b87a347af1d517993fba566ecccdfcb273c8e29
Alvaro Bartolome <alvaro@argilla.io>
2024-04-29 15:03:31 +0200

    Extend `LlamaCppLLM` supported kwargs

--- src/distilabel/llms/llamacpp.py
+++ src/distilabel/llms/llamacpp.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 from pydantic import Field, FilePath, PrivateAttr, validate_call
 
@@ -29,22 +29,30 @@ class LlamaCppLLM(LLM):
     """llama.cpp LLM implementation running the Python bindings for the C++ code.
 
     Attributes:
-        chat_format: the chat format to use for the model. Defaults to `chatml`.
         model_path: contains the path to the GGUF quantized model, compatible with the
             installed version of the `llama.cpp` Python bindings.
         n_gpu_layers: the number of layers to use for the GPU. Defaults to `-1`, meaning that
             the available GPU device will be used.
+        chat_format: the chat format to use for the model. Defaults to `None`, which means the
+            Llama format will be used.
+        n_ctx: the context size to use for the model. Defaults to `512`.
+        n_batch: the prompt processing maximum batch size to use for the model. Defaults to `512`.
+        seed: random seed to use for the generation. Defaults to `4294967295`.
         verbose: whether to print verbose output. Defaults to `False`.
+        extra_kwargs: additional dictionary of keyword arguments that will be passed to the
+            `Llama` class of `llama_cpp` library. Defaults to `{}`.
         _model: the Llama model instance. This attribute is meant to be used internally and
             should not be accessed directly. It will be set in the `load` method.
 
     Runtime parameters:
         - `model_path`: the path to the GGUF quantized model.
         - `n_gpu_layers`: the number of layers to use for the GPU. Defaults to `-1`.
+        - `chat_format`: the chat format to use for the model. Defaults to `None`.
         - `verbose`: whether to print verbose output. Defaults to `False`.
+        - `extra_kwargs`: additional dictionary of keyword arguments that will be passed to the
+            `Llama` class of `llama_cpp` library. Defaults to `{}`.
     """
 
-    chat_format: str = "chatml"
     model_path: RuntimeParameter[FilePath] = Field(
         default=None, description="The path to the GGUF quantized model."
     )
@@ -52,15 +60,31 @@ class LlamaCppLLM(LLM):
         default=-1,
         description="The number of layers that will be loaded in the GPU.",
     )
+    chat_format: Optional[RuntimeParameter[str]] = Field(
+        default=None,
+        description="The chat format to use for the model. Defaults to `None`, which means the Llama format will be used.",
+    )
+
+    n_ctx: int = 512
+    n_batch: int = 512
+    seed: int = 4294967295
+
     verbose: RuntimeParameter[bool] = Field(
         default=False,
         description="Whether to print verbose output from llama.cpp library.",
+    )
+    extra_kwargs: Optional[RuntimeParameter[Dict[str, Any]]] = Field(
+        default_factory=dict,
+        description="Additional dictionary of keyword arguments that will be passed to the"
+        " `Llama` class of `llama_cpp` library. See all the suported arguments at: "
+        "https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.__init__",
     )
 
     _model: Optional["Llama"] = PrivateAttr(...)
 
     def load(self) -> None:
         """Loads the `Llama` model from the `model_path`."""
+        super().load()
 
         try:
             from llama_cpp import Llama
@@ -70,13 +94,15 @@ class LlamaCppLLM(LLM):
             ) from ie
 
         self._model = Llama(
-            model_path=self.model_path.as_posix(),
+            model_path=self.model_path.as_posix(),  # type: ignore
+            seed=self.seed,
+            n_ctx=self.n_ctx,
+            n_batch=self.n_batch,
             chat_format=self.chat_format,
             n_gpu_layers=self.n_gpu_layers,
             verbose=self.verbose,
+            **self.extra_kwargs,
         )
-
-        super().load()
 
     @property
     def model_name(self) -> str:
@@ -93,6 +119,7 @@ class LlamaCppLLM(LLM):
         presence_penalty: float = 0.0,
         temperature: float = 1.0,
         top_p: float = 1.0,
+        extra_generation_kwargs: Optional[Dict[str, Any]] = None,
     ) -> List[GenerateOutput]:
         """Generates `num_generations` responses for the given input using the Llama model.
 
@@ -108,6 +135,9 @@ class LlamaCppLLM(LLM):
                 `0.0`.
             temperature: the temperature to use for the generation. Defaults to `0.1`.
             top_p: the top-p value to use for the generation. Defaults to `1.0`.
+            extra_generation_kwargs: dictionary with additional arguments to be passed to
+                the `create_chat_completion` method. Reference at
+                https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.create_chat_completion
 
         Returns:
             A list of lists of strings containing the generated responses for each input.
@@ -124,6 +154,7 @@ class LlamaCppLLM(LLM):
                         presence_penalty=presence_penalty,
                         temperature=temperature,
                         top_p=top_p,
+                        **(extra_generation_kwargs or {}),
                     )
                 )
                 outputs.append(chat_completions["choices"][0]["message"]["content"])
