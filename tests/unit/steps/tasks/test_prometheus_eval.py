38343cfae8f53443051b2ba5a4a27ff6eb63ee14
Alvaro Bartolome <alvaro@argilla.io>
2024-05-08 08:23:56 +0200

    Update `test_imports` and `test_prometheus_eval`

--- tests/unit/steps/tasks/test_prometheus_eval.py
+++ tests/unit/steps/tasks/test_prometheus_eval.py
@@ -23,11 +23,7 @@ from typing import Any, Dict, Union
 
 import pytest
 from distilabel.pipeline.local import Pipeline
-from distilabel.steps.tasks.prometheus_eval import (
-    _RUBRICS,
-    PrometheusAbsEval,
-    PrometheusRelEval,
-)
+from distilabel.steps.tasks.prometheus_eval import _RUBRICS, PrometheusEval
 from jinja2 import Template
 
 from tests.unit.steps.tasks.utils import DummyLLM
@@ -47,45 +43,91 @@ def load_template(template: str) -> Template:
 
 class TestPrometheusAbsEval:
     @pytest.mark.parametrize(
-        "rubric, reference, template, input",
+        "mode, rubric, reference, template, input",
         [
             (
+                "absolute",
                 "helpfulness",
                 True,
                 "absolute_with_reference.jinja2",
                 {"instruction": "A", "generation": "B", "reference": "C"},
             ),
             (
+                "absolute",
                 "harmlessness",
                 False,
                 "absolute_without_reference.jinja2",
                 {"instruction": "A", "generation": "B"},
             ),
             (
+                "absolute",
                 "honesty",
                 True,
                 "absolute_with_reference.jinja2",
                 {"instruction": "A", "generation": "B", "reference": "C"},
             ),
             (
+                "absolute",
                 "factual-validity",
                 False,
                 "absolute_without_reference.jinja2",
                 {"instruction": "A", "generation": "B"},
             ),
             (
+                "absolute",
                 "reasoning",
                 True,
                 "absolute_with_reference.jinja2",
                 {"instruction": "A", "generation": "B", "reference": "C"},
             ),
+            (
+                "relative",
+                "helpfulness",
+                True,
+                "relative_with_reference.jinja2",
+                {"instruction": "A", "generations": ["B", "C"], "reference": "D"},
+            ),
+            (
+                "relative",
+                "harmlessness",
+                False,
+                "relative_without_reference.jinja2",
+                {"instruction": "A", "generations": ["B", "C"]},
+            ),
+            (
+                "relative",
+                "honesty",
+                True,
+                "relative_with_reference.jinja2",
+                {"instruction": "A", "generations": ["B", "C"], "reference": "D"},
+            ),
+            (
+                "relative",
+                "factual-validity",
+                False,
+                "relative_without_reference.jinja2",
+                {"instruction": "A", "generations": ["B", "C"]},
+            ),
+            (
+                "relative",
+                "reasoning",
+                True,
+                "relative_with_reference.jinja2",
+                {"instruction": "A", "generations": ["B", "C"], "reference": "D"},
+            ),
         ],
     )
     def test_format_input(
-        self, rubric: str, reference: bool, template: str, input: Dict[str, str]
+        self,
+        mode: str,
+        rubric: str,
+        reference: bool,
+        template: str,
+        input: Dict[str, str],
     ) -> None:
-        task = PrometheusAbsEval(
+        task = PrometheusEval(
             name="task",
+            mode=mode,  # type: ignore
             rubric=rubric,  # type: ignore
             reference=reference,
             llm=DummyLLM(),
@@ -101,9 +143,10 @@ class TestPrometheusAbsEval:
         ).render(**template_kwargs)
 
     def test_format_input_errors(self) -> None:
-        # `reference=True` but reference not provided
-        task = PrometheusAbsEval(
+        # any `mode` and `reference=True` but reference not provided
+        task = PrometheusEval(
             name="task",
+            mode="absolute",
             rubric="helpfulness",
             reference=True,
             llm=DummyLLM(),
@@ -114,127 +157,27 @@ class TestPrometheusAbsEval:
         with pytest.raises(KeyError, match="reference"):
             task.format_input(input={"instruction": "A", "generation": "B"})
 
-    @pytest.mark.parametrize(
-        "output, expected",
-        [
-            (
-                "Feedback: A \n[RESULT] 1\n",
-                {"feedback": "A", "result": 1},
-            ),
-            (
-                "Feedback: A [RESULT] 1",
-                {"feedback": "A", "result": 1},
-            ),
-            (
-                "Feedback: A [RESULT] 6",
-                {"feedback": None, "result": None},
-            ),
-            (
-                "A [RESULT] 1",
-                {"feedback": "A", "result": 1},
-            ),
-            (
-                None,
-                {"feedback": None, "result": None},
-            ),
-        ],
-    )
-    def test_format_output(
-        self, output: Union[str, None], expected: Dict[str, Any]
-    ) -> None:
-        task = PrometheusAbsEval(
+        # `mode=absolute` and `generation` is not a string
+        task = PrometheusEval(
             name="task",
-            rubric="factual-validity",
+            mode="absolute",
+            rubric="helpfulness",
             reference=False,
             llm=DummyLLM(),
             pipeline=Pipeline(name="unit-test-pipeline"),
         )
         task.load()
 
-        assert (
-            task.format_output(
-                output=output,
-                input={
-                    "instruction": "A",
-                    "generation": "B",
-                },
-            )
-            == expected
-        )
+        with pytest.raises(
+            ValueError,
+            match=r"Provided \`generation\` is of type \<class 'int'\> but a string should be provided instead.",
+        ):
+            task.format_input(input={"instruction": "A", "generation": 1})
 
-
-class TestPrometheusRelEval:
-    @pytest.mark.parametrize(
-        "rubric, reference, template, input",
-        [
-            (
-                "helpfulness",
-                True,
-                "relative_with_reference.jinja2",
-                {"instruction": "A", "generations": ["B", "C"], "reference": "D"},
-            ),
-            (
-                "harmlessness",
-                False,
-                "relative_without_reference.jinja2",
-                {"instruction": "A", "generations": ["B", "C"]},
-            ),
-            (
-                "honesty",
-                True,
-                "relative_with_reference.jinja2",
-                {"instruction": "A", "generations": ["B", "C"], "reference": "D"},
-            ),
-            (
-                "factual-validity",
-                False,
-                "relative_without_reference.jinja2",
-                {"instruction": "A", "generations": ["B", "C"]},
-            ),
-            (
-                "reasoning",
-                True,
-                "relative_with_reference.jinja2",
-                {"instruction": "A", "generations": ["B", "C"], "reference": "D"},
-            ),
-        ],
-    )
-    def test_format_input(
-        self, rubric: str, reference: bool, template: str, input: Dict[str, str]
-    ) -> None:
-        task = PrometheusRelEval(
+        # `mode=relative` and `generations` is not a list with exactly 2 elements
+        task = PrometheusEval(
             name="task",
-            rubric=rubric,  # type: ignore
-            reference=reference,
-            llm=DummyLLM(),
-            pipeline=Pipeline(name="unit-test-pipeline"),
-        )
-        task.load()
-
-        template_kwargs = input
-        template_kwargs["rubric"] = _RUBRICS[rubric]
-
-        assert task.format_input(input=input)[-1]["content"] == load_template(
-            template=template
-        ).render(**template_kwargs)
-
-    def test_format_input_errors(self) -> None:
-        # `reference=True` but reference not provided
-        task = PrometheusRelEval(
-            name="task",
-            rubric="helpfulness",
-            reference=True,
-            llm=DummyLLM(),
-            pipeline=Pipeline(name="unit-test-pipeline"),
-        )
-        task.load()
-
-        with pytest.raises(KeyError, match="reference"):
-            task.format_input(input={"instruction": "A", "generations": ["B", "C"]})
-
-        # `generations` is not a list with exactly 2 elements
-        task = PrometheusRelEval(
-            name="task",
+            mode="relative",
             rubric="helpfulness",
             reference=False,
             llm=DummyLLM(),
@@ -252,35 +195,66 @@ class TestPrometheusRelEval:
                 )
 
     @pytest.mark.parametrize(
-        "output, expected",
+        "mode, output, expected",
         [
             (
+                "absolute",
+                "Feedback: A \n[RESULT] 1\n",
+                {"feedback": "A", "result": 1},
+            ),
+            (
+                "absolute",
+                "Feedback: A [RESULT] 1",
+                {"feedback": "A", "result": 1},
+            ),
+            (
+                "absolute",
+                "Feedback: A [RESULT] 6",
+                {"feedback": None, "result": None},
+            ),
+            (
+                "absolute",
+                "A [RESULT] 1",
+                {"feedback": "A", "result": 1},
+            ),
+            (
+                "absolute",
+                None,
+                {"feedback": None, "result": None},
+            ),
+            (
+                "relative",
                 "Feedback: A \n[RESULT] 1\n",
                 {"feedback": None, "result": None},
             ),
             (
+                "relative",
                 "Feedback: A [RESULT] A",
                 {"feedback": "A", "result": "A"},
             ),
             (
+                "relative",
                 "Feedback: A [RESULT] B",
                 {"feedback": "A", "result": "B"},
             ),
             (
+                "relative",
                 "A [RESULT] 1",
                 {"feedback": None, "result": None},
             ),
             (
+                "relative",
                 None,
                 {"feedback": None, "result": None},
             ),
         ],
     )
     def test_format_output(
-        self, output: Union[str, None], expected: Dict[str, Any]
+        self, mode: str, output: Union[str, None], expected: Dict[str, Any]
     ) -> None:
-        task = PrometheusRelEval(
+        task = PrometheusEval(
             name="task",
+            mode=mode,  # type: ignore
             rubric="factual-validity",
             reference=False,
             llm=DummyLLM(),
@@ -288,13 +262,4 @@ class TestPrometheusRelEval:
         )
         task.load()
 
-        assert (
-            task.format_output(
-                output=output,
-                input={
-                    "instruction": "A",
-                    "generations": ["B", "C"],
-                },
-            )
-            == expected
-        )
+        assert task.format_output(output=output, input={}) == expected
