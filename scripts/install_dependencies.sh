c7eceb36f45af377438ec6e4a1acb15753b6ed89
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-05-30 20:17:42 +0200

    Update to `argilla >= 1.29.0`

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,11 +2,8 @@
 
 python_version=$(python -c "import sys; print(sys.version_info[:2])")
 
-pip install -e .[dev,tests,anthropic,cohere,groq,hf-inference-endpoints,hf-transformers,litellm,llama-cpp,ollama,openai,outlines,vertexai,vllm]
+pip install -e .[dev,tests,anthropic,argilla,cohere,groq,hf-inference-endpoints,hf-transformers,litellm,llama-cpp,ollama,openai,outlines,vertexai,vllm]
 if [ "${python_version}" != "(3, 8)" ]; then
-	pip install -e .[mistralai]
-fi
-if [ "${python_version}" != "(3, 12)" ]; then
-	pip install -e .[argilla]
+	pip install -e .[mistralai,instructor]
 fi
 pip install git+https://github.com/argilla-io/LLM-Blender.git
