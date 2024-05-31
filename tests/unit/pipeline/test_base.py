ac41e7f884755c5cf3eea59dc47952874a37c7ac
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-05-31 10:26:58 +0200

    Pass data of batches using file system (#678)
    * Refactor `BasePipeline.__init__` method
    
    * Setup `_WriteBuffer` in base and do not cache if `dry_run`
    
    * Add `write_batch` method
    
    * Add methods to write and read from filesystem
    
    * Make `log_queue` optional
    
    * Update unit tests
    
    * Add docs for passing data with file system
    
    * Add integration tests for fs
    
    * Remove integration tests timeout
    
    * Make integration test lighter
    
    * Remove test
    
    * Update `mkdocs.yml`
    
    * Verbose
    
    * Add `tmate` for debugging
    
    * Clean handlers only if not pytest
    
    * Fix loading batch manager
    
    * Add testing fs to pass data again
    
    This reverts commit 9246c4caab6642747e3bad82208e2cd26d5a3579.
    
    * Remove verbose
    
    * Fix test
    
    * Increase tests timeout
    
    * `join` after `terminate`
    
    * Set thread daemon
    
    * Proper termination of manager and pool
    
    * Fix pytest hanging because of queue was never closed and the thread of
    the queue was being kept alive (frustration)
    
    * Terminate pool first and call `stop_logging`
    
    * Remove `protocol`
    
    Co-authored-by: plaguss <plaguss@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: plaguss <plaguss@users.noreply.github.com>

--- tests/unit/pipeline/test_base.py
+++ tests/unit/pipeline/test_base.py
@@ -32,10 +32,19 @@ from distilabel.pipeline.base import (
 )
 from distilabel.pipeline.local import Pipeline
 from distilabel.steps.base import GlobalStep, Step, StepInput
+from distilabel.steps.typing import StepOutput
 from distilabel.utils.serialization import TYPE_INFO_KEY
+from fsspec.implementations.local import LocalFileSystem
 from pydantic import Field
+from upath import UPath
 
-from .utils import DummyGeneratorStep, DummyStep1, DummyStep2, batch_gen
+from .utils import (
+    DummyGeneratorStep,
+    DummyGlobalStep,
+    DummyStep1,
+    DummyStep2,
+    batch_gen,
+)
 
 if TYPE_CHECKING:
     from distilabel.steps.base import GeneratorStep
@@ -69,6 +78,134 @@ class TestBasePipeline:
             assert _GlobalPipelineManager.get_pipeline() == pipeline
 
         assert _GlobalPipelineManager.get_pipeline() is None
+
+    @pytest.mark.parametrize("use_cache", [False, True])
+    def test_load_batch_manager(self, use_cache: bool) -> None:
+        pipeline = BasePipeline(name="unit-test-pipeline")
+        pipeline._load_batch_manager(use_cache=True)
+        pipeline._cache()
+
+        with mock.patch(
+            "distilabel.pipeline.base._BatchManager.load_from_cache"
+        ) as mock_load_from_cache, mock.patch(
+            "distilabel.pipeline.base._BatchManager.from_dag"
+        ) as mock_from_dag:
+            pipeline._load_batch_manager(use_cache=use_cache)
+
+        if use_cache:
+            mock_load_from_cache.assert_called_once_with(
+                pipeline._cache_location["batch_manager"]
+            )
+            mock_from_dag.assert_not_called()
+        else:
+            mock_load_from_cache.assert_not_called()
+            mock_from_dag.assert_called_once_with(pipeline.dag)
+
+    def test_setup_write_buffer(self) -> None:
+        pipeline = BasePipeline(name="unit-test-pipeline")
+
+        pipeline._setup_write_buffer()
+        assert isinstance(pipeline._write_buffer, _WriteBuffer)
+
+    def test_set_logging_parameters(self) -> None:
+        pipeline = BasePipeline(name="unit-test-pipeline")
+        pipeline._set_logging_parameters({"unit-test": "yes"})
+
+        assert pipeline._logging_parameters == {"unit-test": "yes"}
+
+    def test_setup_fsspec(self) -> None:
+        pipeline = BasePipeline(name="unit-test-pipeline")
+
+        with mock.patch("fsspec.filesystem") as mock_filesystem:
+            pipeline._setup_fsspec({"path": "gcs://my-bucket", "extra": "stuff"})
+
+        mock_filesystem.assert_called_once_with("gcs", **{"extra": "stuff"})
+
+    def test_setup_fsspec_default(self) -> None:
+        pipeline = BasePipeline(name="unit-test-pipeline")
+        pipeline._setup_fsspec()
+
+        assert isinstance(pipeline._fs, LocalFileSystem)
+        assert (
+            pipeline._storage_base_path
+            == f"file://{pipeline._cache_location['batch_input_data']}"
+        )
+
+    def test_setup_fsspec_raises_value_error(self) -> None:
+        pipeline = BasePipeline(name="unit-test-pipeline")
+
+        with pytest.raises(ValueError, match="The 'path' key must be present"):
+            pipeline._setup_fsspec({"key": "random"})
+
+    def test_send_batch_to_step(self) -> None:
+        with BasePipeline(name="unit-test-pipeline") as pipeline:
+            generator = DummyGeneratorStep()
+            step = DummyStep1()
+            global_step = DummyGlobalStep()
+
+            generator >> [step, global_step]
+
+        pipeline._batch_manager = mock.MagicMock()
+        pipeline._setup_fsspec()
+
+        with mock.patch(
+            "distilabel.pipeline.base._Batch.write_batch_data_to_fs"
+        ) as mock_write:
+            batch = _Batch(seq_no=0, step_name=generator.name, last_batch=False)  # type: ignore
+            pipeline._send_batch_to_step(batch)
+            pipeline._batch_manager.set_last_batch_sent.assert_called_once_with(batch)
+
+            pipeline._send_batch_to_step(
+                _Batch(seq_no=0, step_name=step.name, last_batch=False)  # type: ignore
+            )
+
+        mock_write.assert_not_called()
+
+        with mock.patch(
+            "distilabel.pipeline.base._Batch.write_batch_data_to_fs"
+        ) as mock_write:
+            pipeline._send_batch_to_step(
+                _Batch(seq_no=0, step_name=global_step.name, last_batch=False)  # type: ignore
+            )
+
+        mock_write.assert_called_once_with(
+            pipeline._fs,
+            UPath(pipeline._storage_base_path) / global_step.name,
+        )
+
+        pipeline._use_fs_to_pass_data = True
+
+        with mock.patch(
+            "distilabel.pipeline.base._Batch.write_batch_data_to_fs"
+        ) as mock_write:
+            pipeline._send_batch_to_step(
+                _Batch(seq_no=0, step_name=generator.name, last_batch=False)  # type: ignore
+            )
+
+        mock_write.assert_not_called()
+
+        with mock.patch(
+            "distilabel.pipeline.base._Batch.write_batch_data_to_fs"
+        ) as mock_write:
+            pipeline._send_batch_to_step(
+                _Batch(seq_no=0, step_name=step.name, last_batch=False)  # type: ignore
+            )
+            pipeline._send_batch_to_step(
+                _Batch(seq_no=0, step_name=global_step.name, last_batch=False)  # type: ignore
+            )
+
+        mock_write.assert_has_calls(
+            [
+                mock.call(
+                    pipeline._fs,
+                    UPath(pipeline._storage_base_path) / step.name,
+                ),
+                mock.call(
+                    pipeline._fs,
+                    UPath(pipeline._storage_base_path) / global_step.name,
+                ),
+            ]
+        )
 
     def test_get_runtime_parameters_info(self) -> None:
         class DummyStep1(Step):
@@ -180,8 +317,8 @@ class TestBasePipeline:
                 default=None, description="runtime_param2 description"
             )
 
-            def process(self, inputs: StepInput) -> None:
-                pass
+            def process(self, inputs: StepInput) -> StepOutput:  # type: ignore
+                yield [{}]
 
         class DummyStep2(Step):
             runtime_param3: RuntimeParameter[str] = Field(
@@ -191,8 +328,8 @@ class TestBasePipeline:
                 default=None, description="runtime_param4 description"
             )
 
-            def process(self, inputs: StepInput) -> None:
-                pass
+            def process(self, inputs: StepInput) -> StepOutput:  # type: ignore
+                yield [{}]
 
         with BasePipeline(name="unit-test-pipeline") as pipeline:
             gen_step = DummyGeneratorStep(name="dummy_generator_step")
@@ -205,7 +342,8 @@ class TestBasePipeline:
         if expected:
             assert expected in caplog.text
         else:
-            assert caplog.text == expected
+            assert "Did you mean any of:" not in expected
+            assert "Available runtime parameters for the step" not in expected
 
     def test_cache_dir_env_variable(self) -> None:
         with mock.patch.dict(os.environ, clear=True):
@@ -2508,62 +2646,6 @@ class TestPipelineSerialization:
 
         signature = pipeline._create_signature()
         assert signature == "a11ac46253598e6fe126420b23b9ad31c6422c92"
-
-    @pytest.mark.parametrize("use_cache", [True, False])
-    def test_run_pipe_and_load_from_cache(self, use_cache: bool):
-        # Maybe not the best place for this test, but does the work for now
-        from distilabel.pipeline.base import BasePipeline
-        from distilabel.pipeline.routing_batch_function import sample_n_steps
-
-        from tests.unit.pipeline.utils import DummyGeneratorStep, DummyStep1, DummyStep2
-
-        sample_two_steps = sample_n_steps(2)
-
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            with BasePipeline(
-                name="unit-test-pipeline", cache_dir=tmpdirname
-            ) as pipeline:
-                dummy_generator = DummyGeneratorStep()
-                dummy_step_1_0 = DummyStep1()
-                dummy_step_1_1 = DummyStep1()
-                dummy_step_1_2 = DummyStep1()
-                dummy_step_2 = DummyStep2()
-
-                (
-                    dummy_generator
-                    >> sample_two_steps
-                    >> [dummy_step_1_0, dummy_step_1_1, dummy_step_1_2]
-                    >> dummy_step_2
-                )
-
-                pipeline.run({}, use_cache=use_cache)
-
-                assert not pipeline._cache_location["pipeline"].exists()
-                # Set the _BatchManager to the pipeline to check it exists afterwards
-                pipeline._batch_manager = _BatchManager.from_dag(pipeline.dag)
-                pipeline._cache()
-
-                assert pipeline._cache_location["pipeline"].exists()
-
-            with BasePipeline(name="unit-test-pipeline", cache_dir=tmpdirname) as pipe:
-                dummy_generator = DummyGeneratorStep()
-                dummy_step_1_0 = DummyStep1()
-                dummy_step_1_1 = DummyStep1()
-                dummy_step_1_2 = DummyStep1()
-                dummy_step_2 = DummyStep2()
-
-                (
-                    dummy_generator
-                    >> sample_two_steps
-                    >> [dummy_step_1_0, dummy_step_1_1, dummy_step_1_2]
-                    >> dummy_step_2
-                )
-
-                pipe.run({}, use_cache=use_cache)
-                if use_cache:
-                    assert pipe._batch_manager
-                else:
-                    assert not pipe._batch_manager
 
     def test_binary_rshift_operator(self) -> None:
         # Tests the steps can be connected using the >> operator.
