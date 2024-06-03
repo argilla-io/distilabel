f3429ff256de71456b8f7745abab6622289659b2
plaguss <agustin@argilla.io>
2024-06-03 15:55:28 +0200

    Add a GeneratorStep to read files from disk as datasets

--- src/distilabel/steps/generators/huggingface.py
+++ src/distilabel/steps/generators/huggingface.py
@@ -13,13 +13,16 @@
 # limitations under the License.
 
 import os
+from collections import defaultdict
 from functools import lru_cache
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
 
 import requests
-from datasets import DatasetInfo, IterableDataset, load_dataset
+from datasets import Dataset, DatasetInfo, IterableDataset, load_dataset
 from pydantic import Field, PrivateAttr
 from requests.exceptions import ConnectionError
+from upath import UPath
 
 from distilabel.mixins.runtime_parameters import RuntimeParameter
 from distilabel.steps.base import GeneratorStep
@@ -28,7 +31,7 @@ if TYPE_CHECKING:
     from distilabel.steps.typing import GeneratorStepOutput
 
 
-class LoadHubDataset(GeneratorStep):
+class LoadFromHub(GeneratorStep):
     """Loads a dataset from the Hugging Face Hub.
 
     `GeneratorStep` that loads a dataset from the Hugging Face Hub using the `datasets`
@@ -80,8 +83,12 @@ class LoadHubDataset(GeneratorStep):
         default=None,
         description="The number of examples to load from the dataset. By default will load all examples.",
     )
+    storage_options: Optional[Dict[str, Any]] = Field(
+        default=None,
+        description="The storage options to use when loading the dataset.",
+    )
 
-    _dataset: Union[IterableDataset, None] = PrivateAttr(...)
+    _dataset: Union[IterableDataset, Dataset, None] = PrivateAttr(...)
 
     def load(self) -> None:
         """Load the dataset from the Hugging Face Hub"""
@@ -232,3 +239,126 @@ def _get_hf_dataset_info(
     ), f"Failed to get '{repo_id}' dataset info. Make sure you have set the HF_TOKEN environment variable if it is a private dataset."
 
     return response.json()["dataset_info"]
+
+
+class LoadFromDisk(LoadFromHub):
+    """Loads a dataset from a file in disk.
+
+    Take a look at [Hugging Face Datasets](https://huggingface.co/docs/datasets/loading)
+    for more information of the supported file types.
+    """
+
+    data_files: Optional[Union[str, Path]] = Field(
+        default=None,
+        description="The data files, or directory containing the data files, to generate the dataset from.",
+    )
+    filetype: Optional[RuntimeParameter[str]] = Field(
+        default=None,
+        description="The expected filetype. If not provided, it will be inferred from the file extension.",
+    )
+
+    def load(self) -> None:
+        """Load the dataset from the file/s in disk."""
+        super(GeneratorStep, self).load()
+
+        data_path = UPath(self.data_files, storage_options=self.storage_options)
+
+        # def get_filetype(data_path: UPath) -> str:
+        #     filetype = data_path.suffix.lstrip(".")
+        #     if filetype == "jsonl":
+        #         filetype = "json"
+        #     return filetype
+
+        # if data_path.is_file():
+        #     self.filetype = get_filetype(data_path)
+        #     data_files = str(data_path)
+        # elif data_path.is_dir():
+        #     file_sequence = []
+        #     file_map = defaultdict(list)
+        #     for file_or_folder in data_path.iterdir():
+        #         if file_or_folder.is_file():
+        #             file_sequence.append(str(file_or_folder))
+        #         elif file_or_folder.is_dir():
+        #             for file in file_or_folder.iterdir():
+        #                 file_sequence.append(str(file))
+        #                 file_map[str(file_or_folder)].append(str(file))
+
+        #     data_files = file_sequence or file_map
+        #     # Try to obtain the filetype from any of the files, assuming all files have the same type.
+        #     if file_sequence:
+        #         self.filetype = get_filetype(UPath(file_sequence[0]))
+        #     else:
+        #         self.filetype = get_filetype(
+        #             UPath(file_map[list(file_map.keys())[0]][0])
+        #         )
+        (data_files, self.filetype) = self._prepare_data_files(data_path)
+
+        self._dataset = load_dataset(
+            self.filetype,
+            data_files=data_files,
+            split=self.split,
+            streaming=self.streaming,
+            storage_options=self.storage_options,
+        )
+
+        if not self.streaming and self.num_examples:
+            self._dataset = self._dataset.select(range(self.num_examples))
+        if not self.num_examples:
+            if self.streaming:
+                # There's no better way to get the number of examples in a streaming dataset,
+                # load it again for the moment.
+                self.num_examples = len(
+                    load_dataset(
+                        self.filetype, data_files=self.data_files, split=self.split
+                    )
+                )
+            else:
+                self.num_examples = len(self._dataset)
+
+    def _prepare_data_files(self, data_path: UPath) -> Tuple[str, str]:
+        """Prepare the loading process by setting the `data_files` attribute."""
+
+        def get_filetype(data_path: UPath) -> str:
+            filetype = data_path.suffix.lstrip(".")
+            if filetype == "jsonl":
+                filetype = "json"
+            return filetype
+
+        if data_path.is_file():
+            filetype = get_filetype(data_path)
+            data_files = str(data_path)
+        elif data_path.is_dir():
+            file_sequence = []
+            file_map = defaultdict(list)
+            for file_or_folder in data_path.iterdir():
+                if file_or_folder.is_file():
+                    file_sequence.append(str(file_or_folder))
+                elif file_or_folder.is_dir():
+                    for file in file_or_folder.iterdir():
+                        file_sequence.append(str(file))
+                        file_map[str(file_or_folder)].append(str(file))
+
+            data_files = file_sequence or file_map
+            # Try to obtain the filetype from any of the files, assuming all files have the same type.
+            if file_sequence:
+                filetype = get_filetype(UPath(file_sequence[0]))
+            else:
+                filetype = get_filetype(UPath(file_map[list(file_map.keys())[0]][0]))
+        return data_files, filetype
+
+    @property
+    def outputs(self) -> List[str]:
+        """
+        The columns that will be generated by this step, based on the datasets from a file
+        in disk.
+
+        Returns:
+            The columns that will be generated by this step.
+        """
+        # We assume there are Dataset/IterableDataset, not it's ...Dict counterparts
+        if self._dataset is Ellipsis:
+            raise ValueError(
+                "Dataset not loaded yet, you must call `load` method first."
+            )
+
+        return self._dataset.column_names
