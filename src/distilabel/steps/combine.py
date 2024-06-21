10b49a2212bb44d01916aa3a861c70b1833ac5e4
plaguss <agustin@argilla.io>
2024-06-21 10:11:06 +0200

    Add step to combine keys in a dict

--- src/distilabel/steps/combine.py
+++ src/distilabel/steps/combine.py
@@ -16,7 +16,7 @@ from typing import TYPE_CHECKING, List, Optional
 
 from typing_extensions import override
 
-from distilabel.pipeline.utils import combine_dicts
+from distilabel.pipeline.utils import combine_dicts, combine_keys
 from distilabel.steps.base import Step, StepInput
 
 if TYPE_CHECKING:
@@ -121,3 +121,80 @@ class CombineColumns(Step):
             merge_keys=self.inputs,
             output_merge_keys=self.outputs,
         )
+
+
+class CombineKeys(Step):
+    """Combines keys from a `StepInput`.
+
+    `CombineKeys` is a `Step` that implements the `process` method that calls the `combine_keys`
+    function to handle and combine keys in a `StepInput`. `CombineKeys` provides two attributes
+    `keys` and `output_keys` to specify the keys to merge and the resulting output key.
+
+    This step can be useful if you have a `Task` that generates instructions for example, and you
+    want to have more examples of those. In such a case, you could for example use another `Task`
+    to multiply your instructions synthetically, what would yield two different keys splitted.
+    Using `CombineKeys` you can merge them and use them as a single column in your dataset for
+    further processing.
+
+    Attributes:
+        columns: List of strings with the names of the columns to merge.
+        output_columns: Optional list of strings with the names of the output columns.
+
+    Input columns:
+        - dynamic (determined by `keys` attribute): The keys to merge.
+
+    Output columns:
+        - dynamic (determined by `keys` and `output_key` attributes): The columns
+            that were merged.
+
+    Examples:
+
+        Combine keys in rows of a dataset:
+
+        ```python
+        from distilabel.steps import CombineKeys
+
+        combiner = CombineKeys(
+            keys=["queries", "multiple_queries"],
+            output_key="queries",
+        )
+        combiner.load()
+
+        result = next(
+            combiner.process(
+                [
+                    {
+                        "queries": "How are you?",
+                        "multiple_queries": ["What's up?", "Everything ok?"]
+                    }
+                ],
+            )
+        )
+        # >>> result
+        # [{'queries': ['How are you?', "What's up?", 'Everything ok?']}]
+        ```
+    """
+
+    keys: List[str]
+    output_key: Optional[str] = None
+
+    @property
+    def inputs(self) -> List[str]:
+        return self.keys
+
+    @property
+    def outputs(self) -> List[str]:
+        return [self.output_key] if self.output_key else ["combined_key"]
+
+    @override
+    def process(self, inputs: StepInput) -> "StepOutput":
+        combined = []
+        for input in inputs:
+            combined.append(
+                combine_keys(
+                    input,
+                    keys=self.keys,
+                    new_key=self.outputs[0],
+                )
+            )
+        yield combined
