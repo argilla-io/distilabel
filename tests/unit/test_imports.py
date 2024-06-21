c27cea2dfdc5d7011be167c3a8f77f6855ab030d
plaguss <agustin@argilla.io>
2024-06-21 10:11:52 +0200

    Add tests

--- tests/unit/test_imports.py
+++ tests/unit/test_imports.py
@@ -39,6 +39,7 @@ def test_imports() -> None:
 
     from distilabel.steps import (
         CombineColumns,
+        CombineKeys,
         ConversationTemplate,
         DeitaFiltering,
         ExpandColumns,
