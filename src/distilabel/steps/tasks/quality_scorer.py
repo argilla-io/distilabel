02916df629846320e0c41ae41a2726781a9e321d
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-07-29 12:55:42 +0200

    Replace property definitions `inputs` `outputs` `quality_scorer`

--- src/distilabel/steps/tasks/quality_scorer.py
+++ src/distilabel/steps/tasks/quality_scorer.py
@@ -23,7 +23,7 @@ else:
 from typing import Any, Dict, List, Union
 
 from jinja2 import Template
-from pydantic import PrivateAttr
+from pydantic import Field, PrivateAttr
 
 from distilabel.steps.tasks.base import Task
 from distilabel.steps.tasks.typing import ChatType
@@ -100,6 +100,17 @@ class QualityScorer(Task):
 
     _template: Union[Template, None] = PrivateAttr(...)
 
+    inputs: List[str] = Field(
+        default=["instruction", "responses"],
+        frozen=True,
+        description="The inputs for the task are 'instruction' and 'responses'.",
+    )
+    outputs: List[str] = Field(
+        default=["scores", "model_name"],
+        frozen=True,
+        description="The output for the task is a list of 'scores' and their 'model_name' containing the quality score for each response in 'responses'.",
+    )
+
     def load(self) -> None:
         """Loads the Jinja2 template."""
         super().load()
@@ -114,11 +125,6 @@ class QualityScorer(Task):
 
         self._template = Template(open(_path).read())
 
-    @property
-    def inputs(self) -> List[str]:
-        """The inputs for the task are `instruction` and `responses`."""
-        return ["instruction", "responses"]
-
     def format_input(self, input: Dict[str, Any]) -> ChatType:  # type: ignore
         """The input is formatted as a `ChatType` assuming that the instruction
         is the first interaction from the user within a conversation."""
@@ -130,12 +136,6 @@ class QualityScorer(Task):
                 ),
             }
         ]
-
-    @property
-    def outputs(self):
-        """The output for the task is a list of `scores` containing the quality score for each
-        response in `responses`."""
-        return ["scores", "model_name"]
 
     def format_output(
         self, output: Union[str, None], input: Dict[str, Any]
