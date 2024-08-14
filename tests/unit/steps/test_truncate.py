729c08eed238554d57820ee7a00e3aa69824108d
plaguss <agustin@argilla.io>
2024-08-14 10:42:09 +0200

    Add tests for TruncateRow

---
+++ tests/unit/steps/test_truncate.py
@@ -0,0 +1,46 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional
+
+import pytest
+from distilabel.steps.truncate import TruncateRow
+
+
+@pytest.mark.parametrize(
+    "max_length, text, tokenizer_name, expected",
+    [
+        (
+            10,
+            "This is a sample text that is longer than 10 characters",
+            None,
+            "This is a ",
+        ),
+        (
+            4,
+            "This is a sample text that is longer than 10 characters",
+            "meta-llama/Meta-Llama-3.1-70B-Instruct",
+            "This is a sample",
+        ),
+    ],
+)
+def test_truncate_row(
+    max_length: int, text: str, tokenizer_name: Optional[str], expected: str
+) -> None:
+    trunc = TruncateRow(
+        column="text", max_length=max_length, tokenizer_name=tokenizer_name
+    )
+    trunc.load()
+
+    assert next(trunc.process([{"text": text}])) == [{"text": expected}]
