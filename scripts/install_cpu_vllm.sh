7a90d3690a1cb8ee64591e3d4fb27fa35acf3a21
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-08-21 12:03:47 +0200

    Use python installed libs

--- scripts/install_cpu_vllm.sh
+++ scripts/install_cpu_vllm.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 echo "Updating system and installing build dependencies..."
 sudo apt-get update -y
 sudo apt-get install -y gcc-12 g++-12 libnuma-dev cmake
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 
-# Get Python version and install development files
-PYTHON_VERSION=$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)
-sudo apt-get install -y python${PYTHON_VERSION}-dev
+echo "Python version:"
+python --version
+
+echo "Python executable location:"
+which python
 
 echo "Installing Python build dependencies..."
 python -m pip install --upgrade pip
@@ -29,7 +31,7 @@ echo "Installing vLLM CPU requirements..."
 python -m pip install -r requirements-cpu.txt --extra-index-url https://download.pytorch.org/whl/cpu
 
 echo "Installing vLLM for CPU..."
-export CMAKE_ARGS="-DPYTHON_EXECUTABLE=$(which python) -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")"
+export CMAKE_ARGS="-DPYTHON_EXECUTABLE=$(which python) -DPYTHON_INCLUDE_DIR=$(python -c "from sysconfig import get_path; print(get_path('include'))") -DPYTHON_LIBRARY=$(python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")"
 echo "CMake args: $CMAKE_ARGS"
 VLLM_TARGET_DEVICE=cpu python setup.py install
 
