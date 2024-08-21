df236360b7e9d593f30a617071a513ce5e091b37
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-08-21 11:27:36 +0200

    Add installing build dependencies

--- scripts/install_cpu_vllm.sh
+++ scripts/install_cpu_vllm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Installing system build dependencies..."
+apt-get update -y
+apt-get install -y gcc-12 g++-12 libnuma-dev
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /usr/bin/g++ g++ /usr/bin/g++-12
+
+echo "Installing Python build dependencies..."
+uv pip install --system wheel packaging ninja "setuptools>=49.4.0" numpy
+
 echo "Cloning 'vllm-project/vllm' GitHub repository..."
 git clone https://github.com/vllm-project/vllm.git
 
