1c96c86707c02c29dc96613e4fe9f1139cc51212
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-09-03 16:52:29 +0200

    Fix import

--- src/distilabel/steps/truncate.py
+++ src/distilabel/steps/truncate.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib
+import importlib.util
 from typing import TYPE_CHECKING, Any, Callable, List, Optional
 
 from typing_extensions import override
