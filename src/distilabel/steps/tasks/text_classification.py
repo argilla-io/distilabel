b562b42a3cb6f326890bdce558627b901ec04f0a
plaguss <agustin@argilla.io>
2024-09-09 15:55:42 +0200

    Added examples to the docstrings

--- src/distilabel/steps/tasks/text_classification.py
+++ src/distilabel/steps/tasks/text_classification.py
@@ -51,11 +51,12 @@ Now, please give me the labels in JSON format, do not include any other text in 
 
 
 class TextClassification(Task):
-    """Classifies text into one or more categories or labels.
+    r"""Classifies text into one or more categories or labels.
 
-    ADD EXAMPLES, THIS IS HIGHLY CUSTOMIZABLE.
-    It uses structured generation as per the reference paper, it can help to generate more
-    concise labels. See section 4.1 in the reference.
+    This task can be used for text classification problems, where the goal is to assign
+    one or multiple labels to a given text.
+    It uses structured generation as per the reference paper by default,
+    it can help to generate more concise labels. See section 4.1 in the reference.
 
     Input columns:
         - text (`str`): The reference text we want to obtain labels for.
@@ -83,6 +84,113 @@ class TextClassification(Task):
             a dictionary with the labels and their descriptions.
         default_label: Default label to use when the text is ambiguous or lacks sufficient information for
             classification. Can be a list in case of multiple labels (n>1).
+
+    Examples:
+        Assigning a sentiment to a text:
+
+        ```python
+        from distilabel.steps.tasks import TextClassification
+        from distilabel.llms.huggingface import InferenceEndpointsLLM
+
+        llm = InferenceEndpointsLLM(
+            model_id="meta-llama/Meta-Llama-3.1-70B-Instruct",
+            tokenizer_id="meta-llama/Meta-Llama-3.1-70B-Instruct",
+        )
+
+        text_classification = TextClassification(
+            llm=llm,
+            context="You are an AI system specialized in assigning sentiment to movies.",
+            available_labels=["positive", "negative"],
+        )
+
+        text_classification.load()
+
+        result = next(
+            text_classification.process(
+                [{"text": "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."}]
+            )
+        )
+        # result
+        # [{'text': 'This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three.',
+        # 'labels': 'positive',
+        # 'distilabel_metadata': {'raw_output_text_classification_0': '{\n    "labels": "positive"\n}',
+        # 'raw_input_text_classification_0': [{'role': 'system',
+        #     'content': 'You are an AI system specialized in generating labels to classify pieces of text. Your sole purpose is to analyze the given text and provide appropriate classification labels.'},
+        #     {'role': 'user',
+        #     'content': '# Instruction\nPlease classify the user query by assigning the most appropriate labels.\nDo not explain your reasoning or provide any additional commentary.\nIf the text is ambiguous or lacks sufficient information for classification, respond with "Unclassified".\nProvide the label that best describes the text.\nYou are an AI system specialized in assigning sentiment to movie the user queries.\n## Labeling the user input\nUse the available labels to classify the user query. Analyze the context of each label specifically:\navailable_labels = [\n    "positive",  # The text shows positive sentiment\n    "negative",  # The text shows negative sentiment\n]\n\n\n## User Query\n```\nThis was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three.\n```\n\n## Output Format\nNow, please give me the labels in JSON format, do not include any other text in your response:\n```\n{\n    "labels": "label"\n}\n```'}]},
+        # 'model_name': 'meta-llama/Meta-Llama-3.1-70B-Instruct'}]
+        ```
+
+        Assigning predefined labels with specified descriptions:
+
+        ```python
+        from distilabel.steps.tasks import TextClassification
+
+        text_classification = TextClassification(
+            llm=llm,
+            n=1,
+            context="Determine the intent of the text.",
+            available_labels={
+                "complaint": "A statement expressing dissatisfaction or annoyance about a product, service, or experience. It's a negative expression of discontent, often with the intention of seeking a resolution or compensation.",
+                "inquiry": "A question or request for information about a product, service, or situation. It's a neutral or curious expression seeking clarification or details.",
+                "feedback": "A statement providing evaluation, opinion, or suggestion about a product, service, or experience. It can be positive, negative, or neutral, and is often intended to help improve or inform.",
+                "praise": "A statement expressing admiration, approval, or appreciation for a product, service, or experience. It's a positive expression of satisfaction or delight, often with the intention of encouraging or recommending."
+            },
+            query_title="Customer Query",
+        )
+
+        text_classification.load()
+
+        result = next(
+            text_classification.process(
+                [{"text": "Can you tell me more about your return policy?"}]
+            )
+        )
+        # result
+        # [{'text': 'Can you tell me more about your return policy?',
+        # 'labels': 'inquiry',
+        # 'distilabel_metadata': {'raw_output_text_classification_0': '{\n    "labels": "inquiry"\n}',
+        # 'raw_input_text_classification_0': [{'role': 'system',
+        #     'content': 'You are an AI system specialized in generating labels to classify pieces of text. Your sole purpose is to analyze the given text and provide appropriate classification labels.'},
+        #     {'role': 'user',
+        #     'content': '# Instruction\nPlease classify the customer query by assigning the most appropriate labels.\nDo not explain your reasoning or provide any additional commentary.\nIf the text is ambiguous or lacks sufficient information for classification, respond with "Unclassified".\nProvide the label that best describes the text.\nDetermine the intent of the text.\n## Labeling the user input\nUse the available labels to classify the user query. Analyze the context of each label specifically:\navailable_labels = [\n    "complaint",  # A statement expressing dissatisfaction or annoyance about a product, service, or experience. It\'s a negative expression of discontent, often with the intention of seeking a resolution or compensation.\n    "inquiry",  # A question or request for information about a product, service, or situation. It\'s a neutral or curious expression seeking clarification or details.\n    "feedback",  # A statement providing evaluation, opinion, or suggestion about a product, service, or experience. It can be positive, negative, or neutral, and is often intended to help improve or inform.\n    "praise",  # A statement expressing admiration, approval, or appreciation for a product, service, or experience. It\'s a positive expression of satisfaction or delight, often with the intention of encouraging or recommending.\n]\n\n\n## Customer Query\n```\nCan you tell me more about your return policy?\n```\n\n## Output Format\nNow, please give me the labels in JSON format, do not include any other text in your response:\n```\n{\n    "labels": "label"\n}\n```'}]},
+        # 'model_name': 'meta-llama/Meta-Llama-3.1-70B-Instruct'}]
+        ```
+
+        Free multi label classification without predefined labels:
+
+        ```python
+        from distilabel.steps.tasks import TextClassification
+
+        text_classification = TextClassification(
+            llm=llm,
+            n=3,
+            context=(
+                "Describe the main themes, topics, or categories that could describe the "
+                "following type of persona."
+            ),
+            query_title="Example of Persona",
+        )
+
+        text_classification.load()
+
+        result = next(
+            text_classification.process(
+                [{"text": "A historian or curator of Mexican-American history and culture focused on the cultural, social, and historical impact of the Mexican presence in the United States."}]
+            )
+        )
+        # result
+        # [{'text': 'A historian or curator of Mexican-American history and culture focused on the cultural, social, and historical impact of the Mexican presence in the United States.',
+        # 'labels': ['Historical Researcher',
+        # 'Cultural Specialist',
+        # 'Ethnic Studies Expert'],
+        # 'distilabel_metadata': {'raw_output_text_classification_0': '{\n    "labels": ["Historical Researcher", "Cultural Specialist", "Ethnic Studies Expert"]\n}',
+        # 'raw_input_text_classification_0': [{'role': 'system',
+        #     'content': 'You are an AI system specialized in generating labels to classify pieces of text. Your sole purpose is to analyze the given text and provide appropriate classification labels.'},
+        #     {'role': 'user',
+        #     'content': '# Instruction\nPlease classify the example of persona by assigning the most appropriate labels.\nDo not explain your reasoning or provide any additional commentary.\nIf the text is ambiguous or lacks sufficient information for classification, respond with "Unclassified".\nProvide a list of 3 labels that best describe the text.\nDescribe the main themes, topics, or categories that could describe the following type of persona.\nUse clear, widely understood terms for labels.Avoid overly specific or obscure labels unless the text demands it.\n\n\n## Example of Persona\n```\nA historian or curator of Mexican-American history and culture focused on the cultural, social, and historical impact of the Mexican presence in the United States.\n```\n\n## Output Format\nNow, please give me the labels in JSON format, do not include any other text in your response:\n```\n{\n    "labels": ["label_0", "label_1", "label_2"]\n}\n```'}]},
+        # 'model_name': 'meta-llama/Meta-Llama-3.1-70B-Instruct'}]
+        ```
     """
 
     system_prompt: Optional[str] = (
@@ -119,6 +227,7 @@ class TextClassification(Task):
         default="User Query",
         description="Title of the query used to show the example/s to classify.",
     )
+    use_default_structured_output: bool = True
 
     _template: Optional[Template] = PrivateAttr(default=None)
 
@@ -253,7 +362,7 @@ class TextClassification(Task):
         self, output: str
     ) -> Dict[str, Union[str, List[str]]]:
         """Parses the structured response, which should correspond to a dictionary
-        with either `positive`, or `positive` and `negative` keys.
+        with the `labels`, and either a string or a list of strings with the labels.
 
         Args:
             output: The output from the `LLM`.
