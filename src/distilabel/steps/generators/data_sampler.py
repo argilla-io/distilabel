94ef973d5babca9b12da7798c32712e7f8e1abff
plaguss <agustin@argilla.io>
2024-09-16 09:23:57 +0200

    Add first version of data sampler, step helper for apigen

---
+++ src/distilabel/steps/generators/data_sampler.py
@@ -0,0 +1,77 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+from itertools import islice
+from typing import TYPE_CHECKING, Any, Dict, List
+
+from pydantic import Field
+from typing_extensions import override
+
+from distilabel.steps.base import GeneratorStep
+
+if TYPE_CHECKING:
+    from distilabel.steps.base import GeneratorStepOutput
+
+
+class DataSampler(GeneratorStep):
+    """Step to sample from a dataset."""
+
+    data: List[Dict[str, Any]] = Field(default_factory=list, exclude=True)
+    size: int = 2  # Number of samples per example
+    samples: int = 100  # Number of examples
+
+    @override
+    def process(self, offset: int = 0) -> "GeneratorStepOutput":  # type: ignore
+        """Yields batches from a list of dictionaries.
+
+        Args:
+            offset: The offset to start the generation from. Defaults to `0`.
+
+        Yields:
+            A list of Python dictionaries as read from the inputs (propagated in batches)
+            and a flag indicating whether the yield batch is the last one.
+        """
+
+        total_samples = 0
+
+        while total_samples < self.samples:
+            batch = []
+            bs = min(self.batch_size, self.samples - total_samples)
+            for _ in range(self.batch_size):
+                choices = random.choices(self.data, k=self.size)
+                choices = self._transform_data(choices)
+                batch.extend(choices)
+            total_samples += bs
+            batch = list(islice(batch, bs))
+            yield (batch, True if total_samples >= self.samples else False)
+            batch = []
+
+    @staticmethod
+    def _transform_data(data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        if not data:
+            return []
+
+        result = {key: [] for key in data[0].keys()}
+
+        for item in data:
+            for key, value in item.items():
+                result[key].append(value)
+
+        return [result]
+
+    @property
+    def outputs(self) -> List[str]:
+        """Returns a list of strings with the names of the columns that the step will generate."""
+        return list(self.data[0].keys())
