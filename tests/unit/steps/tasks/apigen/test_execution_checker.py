f5994d8e7dd35ac8342b37e7d4f70fb68db9286a
plaguss <agustin@argilla.io>
2024-09-17 10:58:29 +0200

    Draft tests for new execution checker

---
+++ tests/unit/steps/tasks/apigen/test_execution_checker.py
@@ -0,0 +1,31 @@
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+
+from distilabel.steps.tasks.apigen.execution_checker import (
+    APIGenExecutionChecker,
+)
+
+SAMPLE_LIB = Path(__file__).parent / "_sample_module.py"
+
+
+class TestAPIGenExecutionChecker:
+    def test_process(self):
+        assert SAMPLE_LIB.exists()
+        task = APIGenExecutionChecker(libpath=SAMPLE_LIB)
+        task.load()
+        # api_gen_execution_checker = APIGenExecutionChecker()
+        # inputs = {}
+        # assert api_gen_execution_checker.process(inputs) == "output"
