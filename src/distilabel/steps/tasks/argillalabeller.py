71da8e6d8e0e39b47cefdbf86b30b8fd05a362fe
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-09-26 12:51:14 +0200

    feat: add ability to forward different questions

--- src/distilabel/steps/tasks/argillalabeller.py
+++ src/distilabel/steps/tasks/argillalabeller.py
@@ -65,14 +65,132 @@ _SPANQUESTION_SETTINGS = SpanQuestionSettings()
 
 class ArgillaLabeller(Task):
     """
-    Base class for all tasks in ArgiLabel.
+    Annotate Argilla records based on input fields, example records and question settings.
+
+    This task is designed to facilitate the annotation of Argilla records by leveraging a pre-trained LLM.
+    It uses a system prompt that guides the LLM to understand the input fields, the question type,
+    and the question settings. The task then formats the input data and generates a response based on the question.
+    The response is validated against the question's value model, and the final suggestion is prepared for annotation.
+
+    Attributes:
+        _template: a Jinja2 template used to format the input for the LLM.
+
+    Input columns:
+        - record (`Record`): The record to be annotated.
+        - fields (`Optional[List[Dict[str, Any]]]`): The list of field settings for the input fields.
+        - question (`Optional[Dict[str, Any]]`): The question settings for the question to be answered.
+        - example_records (`Optional[List[Dict[str, Any]]]`): The few shot example records with responses to be used to answer the question.
+        - guidelines (`Optional[str]`): The guidelines for the annotation task.
+
+    Output columns:
+        - suggestions (`Dict[str, Any]`): The final suggestion for annotation.
+
+    Categories:
+        - text-classification
+        - scorer
+        - text-generation
+
+    References:
+        - [`Argilla: Argilla is a collaboration tool for AI engineers and domain experts to build high-quality datasets`](https://github.com/argilla-io/argilla/)
+
+    Examples:
+        Annotate a record with the same dataset and question:
+
+        ```python
+        import argilla as rg
+        from distilabel.steps.tasks import ArgillaLabeller
+        from distilabel.llms.huggingface import InferenceEndpointsLLM
+
+        # Consider this as a placeholder for your actual LLM.
+        labeller = ArgillaLabeller(
+            llm=InferenceEndpointsLLM(
+                model_id="mistralai/Mistral-7B-Instruct-v0.2",
+            )
+            fields=[rg.TextField(name="text")],
+            question=rg.LabelQuestion(name="label", options=["positive", "negative"]),
+            example_records=[rg.Record(fields={"text": "This is a test example record"}, responses={"label": rg.Response(question_name="label", value="positive")})],
+            guidelines="These are the guidelines for the annotation task."
+        )
+
+        labeller.load()
+
+        result = next(
+            labeller.process(
+                [
+                    {
+                        "record": rg.Record(fields={"text": "This is a test record"}).to_dict(),
+                    }
+                ]
+            )
+        )
+        ```
+
+        Annotate a record with alternating datasets and questions:
+
+        ```python
+        import argilla as rg
+        from distilabel.steps.tasks import ArgillaLabeller
+        from distilabel.llms.huggingface import InferenceEndpointsLLM
+
+        # Consider this as a placeholder for your actual LLM.
+        labeller = ArgillaLabeller(
+            llm=InferenceEndpointsLLM(
+                model_id="mistralai/Mistral-7B-Instruct-v0.2",
+            )
+        )
+
+        labeller.load()
+
+        result = next(
+            labeller.process(
+                [
+                    {
+                        "record": rg.Record(fields={"text": "This is a test record"}).to_dict(),
+                        "fields": [rg.TextField(name="text")],
+                        "question": rg.LabelQuestion(name="label", options=["positive", "negative"]),
+                        "example_records": [
+                            rg.Record(
+                                fields={"text": "This is a test example record"},
+                                responses={"label": rg.Response(question_name="label", value="positive")}
+                            )
+                        ],
+                        "guidelines": "These are the guidelines for the annotation task."
+                    }
+                ]
+            )
+        )
+        ```
+
+        Overwrite default prompts and instructions:
+
+        ```python
+        import argilla as rg
+        from distilabel.steps.tasks import ArgillaLabeller
+        from distilabel.llms.huggingface import InferenceEndpointsLLM
+
+        labeller = ArgillaLabeller(
+            llm=InferenceEndpointsLLM(
+                model_id="mistralai/Mistral-7B-Instruct-v0.2",
+            ),
+            system_prompt="You are an expert annotator and labelling assistant that understands complex domains and natural language processing.",
+            question_to_label_instruction={
+                "label": "Select the appropriate label from the list of provided labels.",
+                "multilabel": "Select none, one or multiple labels from the list of provided labels.",
+                "text": "Provide a text response to the question.",
+                "rating": "Provide a rating for the question.",
+                "span": "Provide a list of none, one or multiple spans containing of an exact text from the input field value and a label from the list of provided labels.",
+            },
+        )
+
+        labeller.load()
+        ```
     """
 
     system_prompt: str = (
         "You are an expert annotator and labelling assistant that understands complex domains and natural language processing. "
         "You are given input fields and a question. "
         "You should create a valid JSON object as an answer to the question based on the input fields. "
-        "1. Understand the input field. "
+        "1. Understand the input fields. "
         "2. Understand the question type and the question settings. "
         "3. Reason through your response step-by-step. "
         "4. Provide a valid JSON object as an answer to the question."
@@ -84,7 +202,6 @@ class ArgillaLabeller(Task):
         _RATINGQUESTION_SETTINGS.type: "Provide a rating for the question.",
         _SPANQUESTION_SETTINGS.type: "Provide a list of none, one or multiple spans containing of an exact text from the input field value and a label from the list of provided labels.",
     }
-
     example_records: Optional[
         RuntimeParameter[Union[List[Union[Dict[str, Any], Record]], None]]
     ] = Field(
@@ -113,6 +230,10 @@ class ArgillaLabeller(Task):
         default=None,
         description="The question settings for the question to be answered.",
     )
+    guidelines: Optional[RuntimeParameter[str]] = Field(
+        default=None,
+        description="The guidelines for the annotation task.",
+    )
 
     _template: Union[Template, None] = PrivateAttr(...)
     _client: Optional[Any] = PrivateAttr(None)
@@ -137,11 +258,20 @@ class ArgillaLabeller(Task):
 
     @property
     def optional_inputs(self) -> List[str]:
-        return ["fields", "question", "example_records"]
+        return ["fields", "question", "example_records", "guidelines"]
 
     def _format_record(
         self, record: Dict[str, Any], fields: List[Dict[str, Any]]
     ) -> str:
+        """Format the record fields into a string.
+
+        Args:
+            record (Dict[str, Any]): The record to format.
+            fields (List[Dict[str, Any]]): The fields to format.
+
+        Returns:
+            str: The formatted record fields.
+        """
         output = []
         for field in fields:
             if title := field.get("title"):
@@ -152,10 +282,26 @@ class ArgillaLabeller(Task):
         return "\n".join(output)
 
     def _get_label_instruction(self, question: Dict[str, Any]) -> str:
+        """Get the label instruction for the question.
+
+        Args:
+            question (Dict[str, Any]): The question to get the label instruction for.
+
+        Returns:
+            str: The label instruction for the question.
+        """
         question_type = question["settings"]["type"]
         return self.question_to_label_instruction[question_type]
 
     def _format_question(self, question: Dict[str, Any]) -> str:
+        """Format the question settings into a string.
+
+        Args:
+            question (Dict[str, Any]): The question to format.
+
+        Returns:
+            str: The formatted question.
+        """
         output = [
             f"title: {question.get('title', '')}",
             f"description: {question.get('description', '')}",
@@ -176,6 +322,16 @@ class ArgillaLabeller(Task):
         fields: List[Dict[str, Any]],
         question: Dict[str, Any],
     ) -> str:
+        """Format the example records into a string.
+
+        Args:
+            records (List[Dict[str, Any]]): The records to format.
+            fields (List[Dict[str, Any]]): The fields to format.
+            question (Dict[str, Any]): The question to format.
+
+        Returns:
+            str: The formatted example records.
+        """
         base = []
         for record in records:
             responses = record.get("responses", {})
@@ -210,10 +366,19 @@ class ArgillaLabeller(Task):
             ],
         ],
     ) -> ChatType:
+        """Format the input into a chat message.
+
+        Args:
+            input (Dict[str, Union[Dict[str, Any], Record, TextField, MultiLabelQuestion, LabelQuestion, RatingQuestion, SpanQuestion, TextQuestion]]): The input to format.
+
+        Returns:
+            ChatType: The formatted chat message.
+        """
         record = input[self.inputs[0]]
         fields = input.get(self.optional_inputs[0], self.fields)
         question = input.get(self.optional_inputs[1], self.question)
         examples = input.get(self.optional_inputs[2], self.example_records)
+        guidelines = input.get(self.optional_inputs[3], self.guidelines)
 
         if any([fields is None, question is None]):
             raise ValueError(
@@ -246,6 +411,7 @@ class ArgillaLabeller(Task):
             fields=formatted_fields,
             question=formatted_question,
             examples=formatted_examples,
+            guidelines=guidelines,
         )
 
         messages = []
@@ -261,6 +427,15 @@ class ArgillaLabeller(Task):
     def format_output(
         self, output: Union[str, None], input: Dict[str, Any]
     ) -> Dict[str, Any]:
+        """Format the output into a dictionary.
+
+        Args:
+            output (Union[str, None]): The output to format.
+            input (Dict[str, Any]): The input to format.
+
+        Returns:
+            Dict[str, Any]: The formatted output.
+        """
         fields = input.get("fields", self.fields)
         question = input.get("question", self.question) or self.question
         question = question.serialize() if not isinstance(question, dict) else question
@@ -281,32 +456,67 @@ class ArgillaLabeller(Task):
 
     @override
     def process(self, inputs: StepInput) -> "StepOutput":
-        question = inputs[0].get("question", self.question)
-        question = question.serialize() if not isinstance(question, dict) else question
-        structured_output = {
-            "format": "json",
-            "schema": self._get_pydantic_model_of_structured_output(question),
-        }
-        runtime_parameters = self.llm._runtime_parameters
-        if (
-            "structured_output" in runtime_parameters
-            and runtime_parameters.get("structured_output") != structured_output
-        ):
+        """Process the input through the task.
+
+        Args:
+            inputs (StepInput): The input to process.
+
+        Returns:
+            StepOutput: The output of the task.
+        """
+        questions = [input.get("question", self.question) for input in inputs]
+        questions = [
+            question.serialize() if not isinstance(question, dict) else question
+            for question in questions
+        ]
+        if not all(question == questions[0] for question in questions):
             warnings.warn(
-                "Structured output is handled by ArgillaLabeler internally. Setting structured output to json with schema.",
+                "Not all questions are the same. Processing each question separately by setting the structured output for each question. This may impact performance.",
                 stacklevel=2,
             )
-        runtime_parameters.update(
-            {
-                "structured_output": structured_output,
-            }
-        )
-        self.llm.set_runtime_parameters(runtime_parameters)
-        yield from super().process(inputs)
+            for input, question in zip(inputs, questions):
+                runtime_parameters = self.llm._runtime_parameters
+                runtime_parameters.update(
+                    {
+                        "structured_output": {
+                            "format": "json",
+                            "schema": self._get_pydantic_model_of_structured_output(
+                                question
+                            ),
+                        },
+                    }
+                )
+                self.llm.set_runtime_parameters(runtime_parameters)
+                yield from super().process([input])
+        else:
+            question = questions[0]
+            runtime_parameters = self.llm._runtime_parameters
+            runtime_parameters.update(
+                {
+                    "structured_output": {
+                        "format": "json",
+                        "schema": self._get_pydantic_model_of_structured_output(
+                            question
+                        ),
+                    },
+                }
+            )
+            self.llm.set_runtime_parameters(runtime_parameters)
+            yield from super().process(inputs)
 
     def _resolve_spans(
         self, spans: list, field: str, question: Dict[str, Any]
     ) -> List[Dict[str, Any]]:
+        """Resolve the spans into a list of dictionaries.
+
+        Args:
+            spans (list): The spans to resolve.
+            field (str): The field to resolve the spans for.
+            question (Dict[str, Any]): The question to resolve the spans for.
+
+        Returns:
+            List[Dict[str, Any]]: The resolved spans.
+        """
         unique_spans = {span.exact_text: span.label for span in spans}
         formatted_spans = []
 
@@ -331,6 +541,14 @@ class ArgillaLabeller(Task):
     def _get_value_from_question_value_model(
         self, question_value_model: BaseModel
     ) -> Any:
+        """Get the value from the question value model.
+
+        Args:
+            question_value_model (BaseModel): The question value model to get the value from.
+
+        Returns:
+            Any: The value from the question value model.
+        """
         for attr in ["label", "labels", "spans", "rating", "text"]:
             if hasattr(question_value_model, attr):
                 return getattr(question_value_model, attr)
@@ -339,6 +557,15 @@ class ArgillaLabeller(Task):
     def _assign_value_to_question_value_model(
         self, value: Any, question: Dict[str, Any]
     ) -> BaseModel:
+        """Assign the value to the question value model.
+
+        Args:
+            value (Any): The value to assign.
+            question (Dict[str, Any]): The question to assign the value to.
+
+        Returns:
+            BaseModel: The question value model with the assigned value.
+        """
         question_value_model = self._get_pydantic_model_of_structured_output(question)
         for attr in ["label", "labels", "spans", "rating", "text"]:
             try:
@@ -353,6 +580,14 @@ class ArgillaLabeller(Task):
         self,
         question: Dict[str, Any],
     ) -> BaseModel:
+        """Get the Pydantic model of the structured output.
+
+        Args:
+            question (Dict[str, Any]): The question to get the Pydantic model of the structured output for.
+
+        Returns:
+            BaseModel: The Pydantic model of the structured output.
+        """
         question_type = question["settings"]["type"]
         if question_type == _SPANQUESTION_SETTINGS.type:
 
