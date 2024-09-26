f363292c77fb513517fbbb45b708a0a480523798
plaguss <agustin@argilla.io>
2024-09-26 17:28:36 +0200

    Add setuptools-scm in the script with the dependencies to install it prior to vllm

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,6 +6,7 @@ python_version=$(python -c "import sys; print(sys.version_info[:2])")
 
 python -m pip install uv
 
+uv pip install setuptools-scm
 uv pip install --system -e ".[anthropic,argilla,cohere,groq,hf-inference-endpoints,hf-transformers,litellm,llama-cpp,ollama,openai,outlines,vertexai,mistralai,instructor,sentence-transformers,faiss-cpu,minhash,text-clustering]"
 
 if [ "${python_version}" != "(3, 12)" ]; then
