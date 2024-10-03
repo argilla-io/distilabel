041f796351a92d9495662d269aef16b9c05fdcda
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-10-03 12:54:16 +0200

    Do not install `uv==0.4.18`

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,7 +4,7 @@ set -e
 
 python_version=$(python -c "import sys; print(sys.version_info[:2])")
 
-python -m pip install uv
+python -m pip install --upgrade 'uv!=0.4.18'
 
 uv pip install --system -e ".[anthropic,argilla,cohere,groq,hf-inference-endpoints,hf-transformers,litellm,llama-cpp,ollama,openai,outlines,vertexai,mistralai,instructor,sentence-transformers,faiss-cpu,minhash,text-clustering]"
 
