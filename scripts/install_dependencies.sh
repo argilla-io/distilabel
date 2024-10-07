e99b4fbd0e34755be2018849639bb7566d6f8dbc
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-10-07 12:38:56 +0200

    remove llm blender install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,14 +6,12 @@ python_version=$(python -c "import sys; print(sys.version_info[:2])")
 
 python -m pip install uv
 
-./scripts/install_cpu_vllm.sh
-
 uv pip install --system -e ".[anthropic,argilla,cohere,groq,hf-inference-endpoints,hf-transformers,litellm,llama-cpp,ollama,openai,outlines,vertexai,mistralai,instructor,sentence-transformers,faiss-cpu,minhash,text-clustering]"
 
 if [ "${python_version}" != "(3, 12)" ]; then
     uv pip install --system -e .[ray]
 fi
 
-uv pip install --system git+https://github.com/argilla-io/LLM-Blender.git
+./scripts/install_cpu_vllm.sh
 
 uv pip install --system -e ".[dev,tests]"
