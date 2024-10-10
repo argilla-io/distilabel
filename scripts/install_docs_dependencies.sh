d2724190a3a3f7a1584728f2edacd90798c455f3
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-10-10 15:18:35 +0200

    Update

--- scripts/install_docs_dependencies.sh
+++ scripts/install_docs_dependencies.sh
@@ -4,6 +4,6 @@ set -e
 
 python_version=$(python -c "import sys; print(sys.version_info[:2])")
 
-python -m pip install --break-system-packages uv
+python -m pip install uv
 
 uv pip install --system -e ".[docs]"
