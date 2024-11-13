b2d858a17fddb52731977abe6cf3b1742d50a182
plaguss <agustin@argilla.io>
2024-11-13 17:02:43 +0100

    First version of text-to-image with inference endpoints

--- src/distilabel/models/vlms/huggingface/inference_endpoints.py
+++ src/distilabel/models/vlms/huggingface/inference_endpoints.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 import base64
-from io import BytesIO
-from typing import TYPE_CHECKING, Optional
+import io
+from typing import TYPE_CHECKING, Any, Optional
 
 from pydantic import validate_call
 
@@ -25,21 +25,55 @@ if TYPE_CHECKING:
 
 
 class InferenceEndpointsImageLLM(InferenceEndpointsLLM):
+    """OpenAI image generation implementation running the async API client.
+
+    Attributes:
+        model: the model name to use for the LLM e.g. "dall-e-3", etc.
+            Supported models can be found [here](https://platform.openai.com/docs/guides/images).
+        base_url: the base URL to use for the OpenAI API requests. Defaults to `None`, which
+            means that the value set for the environment variable `OPENAI_BASE_URL` will
+            be used, or "https://api.openai.com/v1" if not set.
+        api_key: the API key to authenticate the requests to the OpenAI API. Defaults to
+            `None` which means that the value set for the environment variable `OPENAI_API_KEY`
+            will be used, or `None` if not set.
+        max_retries: the maximum number of times to retry the request to the API before
+            failing. Defaults to `6`.
+        timeout: the maximum time in seconds to wait for a response from the API. Defaults
+            to `120`.
+
+    Icon:
+        `:hugging:`
+
+    Examples:
+        Generate images from text prompts:
+
+        ```python
+        from distilabel.models.vlms import InferenceEndpointsImageLLM
+
+        llm = InferenceEndpointsImageLLM(model="black-forest-labs/FLUX.1-schnell", api_key="api.key")
+        llm.load()
+
+        output = llm.generate_outputs(
+            inputs=[{"prompt": "a white siamese cat"}],
+        )
+        # {"images": ["iVBORw0KGgoAAAANSUhEUgA..."]}
+        ```
+    """
+
     @validate_call
     async def agenerate(
         self,
-        input: dict[str, any],
+        input: dict[str, Any],
         negative_prompt: Optional[str] = None,
         height: Optional[float] = None,
         width: Optional[float] = None,
         num_inference_steps: Optional[float] = None,
         guidance_scale: Optional[float] = None,
-    ) -> list[dict[str, any]]:
-        """Generates images from text prompts using `huggingface_hub.AsyncClient.text_to_image`.
+    ) -> dict[str, list[str]]:
+        """Generates images from text prompts using `huggingface_hub.AsyncInferenceClient.text_to_image`.
 
         Args:
-            input: Input containing a dict with the key "prompt", with.
-                the prompt to generate an image from.
+            input: Input containing a dict with the key "prompt", with the prompt to generate an image from.
             negative_prompt: An optional negative prompt for the image generation. Defaults to None.
             height: The height in pixels of the image to generate.
             width: The width in pixels of the image to generate.
@@ -49,64 +83,18 @@ class InferenceEndpointsImageLLM(InferenceEndpointsLLM):
                 linked to the text `prompt`, usually at the expense of lower image quality.
 
         Returns:
-            TODO: SHOULD BE THE IMAGE, OR A LIST WITH DICT AND THE IMAGE THERE?
+            A dictionary containing a list with the image as a base64 string.
         """
 
-        prompt = input.get("prompt", "")
         image: "Image" = await self._aclient.text_to_image(
-            prompt,
+            input["prompt"],
             negative_prompt=negative_prompt,
             height=height,
             width=width,
             num_inference_steps=num_inference_steps,
             guidance_scale=guidance_scale,
         )
-
-        buffered = BytesIO()
+        buffered = io.BytesIO()
         image.save(buffered, format="JPEG")
         img_str = base64.b64encode(buffered.getvalue()).decode()
-        return [{"image": img_str}]
-
-
-class InferenceEndpointsImageToImageLLM(InferenceEndpointsLLM):
-    @validate_call
-    async def agenerate(
-        self,
-        input: dict[str, any],
-        negative_prompt: Optional[str] = None,
-        height: Optional[float] = None,
-        width: Optional[float] = None,
-        num_inference_steps: Optional[float] = None,
-        guidance_scale: Optional[float] = None,
-    ) -> list[dict[str, any]]:
-        """Generates images from text prompts using `huggingface_hub.AsyncClient.text_to_image`.
-
-        Args:
-            input: Input containing a dict with the key "prompt", with.
-                the prompt to generate an image from.
-            negative_prompt: An optional negative prompt for the image generation. Defaults to None.
-            height: The height in pixels of the image to generate.
-            width: The width in pixels of the image to generate.
-            num_inference_steps: The number of denoising steps. More denoising steps usually lead
-                to a higher quality image at the expense of slower inference.
-            guidance_scale: Higher guidance scale encourages to generate images that are closely
-                linked to the text `prompt`, usually at the expense of lower image quality.
-
-        Returns:
-            TODO: SHOULD BE THE IMAGE, OR A LIST WITH DICT AND THE IMAGE THERE?
-        """
-
-        prompt = input.get("prompt", "")
-        image: "Image" = await self._aclient.text_to_image(
-            prompt,
-            negative_prompt=negative_prompt,
-            height=height,
-            width=width,
-            num_inference_steps=num_inference_steps,
-            guidance_scale=guidance_scale,
-        )
-
-        buffered = BytesIO()
-        image.save(buffered, format="JPEG")
-        img_str = base64.b64encode(buffered.getvalue()).decode()
-        return [{"image": img_str}]
+        return {"images": [img_str]}
