b201baf974d2065cd45f765c3aa09d6500ff0601
plaguss <agustin@argilla.io>
2024-11-14 11:55:00 +0100

    Redirect imports

--- src/distilabel/models/vlms/__init__.py
+++ src/distilabel/models/vlms/__init__.py
@@ -12,3 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from distilabel.models.vlms.huggingface.inference_endpoints import (
+    InferenceEndpointsImageLLM,
+)
+from distilabel.models.vlms.openai import OpenAIImageLM
+
+__all__ = [
+    "InferenceEndpointsImageLLM",
+    "OpenAIImageLM",
+]
