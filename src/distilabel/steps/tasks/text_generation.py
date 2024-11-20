e603bdf8a9a89d23c5ca64ee9085e48bf4461b21
plaguss <agustin@argilla.io>
2024-11-20 16:56:26 +0100

    Move inner function to be reused for vision

--- src/distilabel/steps/tasks/text_generation.py
+++ src/distilabel/steps/tasks/text_generation.py
@@ -218,23 +218,6 @@ class TextGeneration(Task):
     def load(self) -> None:
         super().load()
 
-        def check_column_in_template(column, template):
-            pattern = (
-                r"(?:{%.*?\b"
-                + re.escape(column)
-                + r"\b.*?%}|{{\s*"
-                + re.escape(column)
-                + r"\s*}})"
-            )
-            if not re.search(pattern, template):
-                raise DistilabelUserError(
-                    (
-                        f"You required column name '{column}', but is not present in the template, "
-                        "ensure the 'columns' match with the 'template' to avoid errors."
-                    ),
-                    page="components-gallery/tasks/textgeneration/",
-                )
-
         for column in self.columns:
             check_column_in_template(column, self.template)
 
@@ -394,3 +377,33 @@ class ChatGeneration(Task):
         """The output is formatted as a dictionary with the `generation`. The `model_name`
         will be automatically included within the `process` method of `Task`."""
         return {"generation": output}
+
+
+def check_column_in_template(
+    column: str, template: str, page: str = "components-gallery/tasks/textgeneration/"
+) -> None:
+    """Checks if a column is present in the template, and raises an error if it isn't.
+
+    Args:
+        column: The column name to check in the template.
+        template: The template of the Task to be checked, the input from the user.
+        page: The page to redirect the user for help . Defaults to "components-gallery/tasks/textgeneration/".
+
+    Raises:
+        DistilabelUserError: Custom error if the column is not present in the template.
+    """
+    pattern = (
+        r"(?:{%.*?\b"
+        + re.escape(column)
+        + r"\b.*?%}|{{\s*"
+        + re.escape(column)
+        + r"\s*}})"
+    )
+    if not re.search(pattern, template):
+        raise DistilabelUserError(
+            (
+                f"You required column name '{column}', but is not present in the template, "
+                "ensure the 'columns' match with the 'template' to avoid errors."
+            ),
+            page=page,
+        )
