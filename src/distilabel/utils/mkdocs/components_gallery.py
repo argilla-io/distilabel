e534a4bedf742d5b776a583f23b0cd68421821de
Agus <agustin@argilla.io>
2025-02-27 15:35:59 +0100

    Update pre commit (#1124)
    Co-authored-by: agus <agustin.piqueres@huggingface.com>

--- src/distilabel/utils/mkdocs/components_gallery.py
+++ src/distilabel/utils/mkdocs/components_gallery.py
@@ -315,9 +315,9 @@ class ComponentsGalleryPlugin(BasePlugin[ComponentsGalleryConfig]):
                 docstring["icon"] = _STEPS_CATEGORY_TO_ICON.get(first_category, "")
 
             if docstring["icon"]:
-                assert (
-                    docstring["icon"] in _STEPS_CATEGORY_TO_ICON.values()
-                ), f"Icon {docstring['icon']} not found in _STEPS_CATEGORY_TO_ICON"
+                assert docstring["icon"] in _STEPS_CATEGORY_TO_ICON.values(), (
+                    f"Icon {docstring['icon']} not found in _STEPS_CATEGORY_TO_ICON"
+                )
 
             name = step["name"]
 
@@ -383,9 +383,9 @@ class ComponentsGalleryPlugin(BasePlugin[ComponentsGalleryConfig]):
                 first_category = docstring["categories"][0]
                 docstring["icon"] = _STEPS_CATEGORY_TO_ICON.get(first_category, "")
             if docstring["icon"]:
-                assert (
-                    docstring["icon"] in _STEPS_CATEGORY_TO_ICON.values()
-                ), f"Icon {docstring['icon']} not found in _STEPS_CATEGORY_TO_ICON"
+                assert docstring["icon"] in _STEPS_CATEGORY_TO_ICON.values(), (
+                    f"Icon {docstring['icon']} not found in _STEPS_CATEGORY_TO_ICON"
+                )
 
             name = task["name"]
 
